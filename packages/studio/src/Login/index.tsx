e00628d52573fa8d6b9e264b4c7346bcaa66e745
Water.Li <lizongbin@hotmail.com>
2022-07-24 16:41:00 +0800

    backup

--- packages/studio/src/Login/index.tsx
+++ packages/studio/src/Login/index.tsx
@@ -1,9 +1,127 @@
 import React from "react"
 import { memo } from "react"
+import { createForm } from '@formily/core'
+import { createSchemaField } from '@formily/react'
+import { Form, FormItem, Input, Password, Submit } from '@formily/antd'
+import { Card } from 'antd'
+import * as ICONS from '@ant-design/icons'
+import { VerifyCode } from './VerifyCode'
 
-const Login = memo(()=>{
+const normalForm = createForm({
+  validateFirst: true,
+})
+
+const phoneForm = createForm({
+  validateFirst: true,
+})
+
+const SchemaField = createSchemaField({
+  components: {
+    FormItem,
+    Input,
+    Password,
+    VerifyCode,
+  },
+  scope: {
+    icon(name) {
+      return React.createElement(ICONS[name])
+    },
+  },
+})
+
+const normalSchema = {
+  type: 'object',
+  properties: {
+    username: {
+      type: 'string',
+      title: '用户名',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {
+        prefix: "{{icon('UserOutlined')}}",
+      },
+    },
+    password: {
+      type: 'string',
+      title: '密码',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'Password',
+      'x-component-props': {
+        prefix: "{{icon('LockOutlined')}}",
+      },
+    },
+  },
+}
+
+const phoneSchema = {
+  type: 'object',
+  properties: {
+    phone: {
+      type: 'string',
+      title: '手机号',
+      required: true,
+      'x-validator': 'phone',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {
+        prefix: "{{icon('PhoneOutlined')}}",
+      },
+    },
+    verifyCode: {
+      type: 'string',
+      title: '验证码',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'VerifyCode',
+      'x-component-props': {
+        prefix: "{{icon('LockOutlined')}}",
+      },
+      'x-reactions': [
+        {
+          dependencies: ['.phone#value', '.phone#valid'],
+          fulfill: {
+            state: {
+              'component[1].readyPost': '{{$deps[0] && $deps[1]}}',
+              'component[1].phoneNumber': '{{$deps[0]}}',
+            },
+          },
+        },
+      ],
+    },
+  },
+}
+
+const Login = memo(() => {
+  const height = "500px"
   return (
-    <div>Login</div>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      background: "url(/public/img/background2.jpg)",
+      height: "100vh",
+      backgroundPosition: " 50%",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+    }}>
+      <Card style={{ width: 400 }}>
+        <h3>登录</h3>
+        <Form
+          form={normalForm}
+          layout="vertical"
+          size="large"
+          onAutoSubmit={console.log}
+        >
+          <SchemaField schema={normalSchema} />
+          <Submit block size="large">
+            登录
+          </Submit>
+        </Form>
+      </Card>
+    </div>
   )
 })
 
