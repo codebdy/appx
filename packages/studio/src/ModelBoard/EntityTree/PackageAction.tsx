63cb47768d40a8b5b19483a8522caed8ac189fbd
Water.Li <lizongbin@hotmail.com>
2022-07-25 18:19:47 +0800

    Update PackageAction.tsx

--- packages/studio/src/ModelBoard/EntityTree/PackageAction.tsx
+++ packages/studio/src/ModelBoard/EntityTree/PackageAction.tsx
@@ -4,26 +4,57 @@ import React, { memo, useCallback, useMemo } from "react"
 import { getLocalMessage } from "../../locales/getLocalMessage";
 import { useSelectedAppId } from "../hooks/useSelectedAppId";
 import { useSetRecoilState } from 'recoil';
-import { packagesState } from "../recoil/atoms";
+import { packagesState, classesState, selectedDiagramState } from "../recoil/atoms";
 import { PackageMeta } from "../meta/PackageMeta";
 import { useDeletePackage } from './../hooks/useDeletePackage';
+import { useCreateNewClass } from "../hooks/useCreateNewClass";
+import { useCreateNewDiagram } from "../hooks/useCreateNewDiagram";
+import { StereoType } from "../meta/ClassMeta";
+import { useBackupSnapshot } from "../hooks/useBackupSnapshot";
 
 const PackageAction = memo((
   props: {
     pkg: PackageMeta,
     onEdit: () => void,
-    onVisibleChange: (visible:boolean) => void,
+    onVisibleChange: (visible: boolean) => void,
   }
 ) => {
   const { pkg, onEdit, onVisibleChange } = props;
   const appId = useSelectedAppId()
   const setPackages = useSetRecoilState(packagesState(appId))
   const deletePackage = useDeletePackage(appId)
-
+  const createNewClass = useCreateNewClass(appId);
+  const createNewDiagram = useCreateNewDiagram(appId);
+  const setClasses = useSetRecoilState(classesState(appId));
+  const backupSnapshot = useBackupSnapshot(appId);
+  const setSelectedDiagram = useSetRecoilState(
+    selectedDiagramState(appId)
+  );
+  
   const handleDelete = useCallback(() => {
     deletePackage(pkg.uuid)
     onVisibleChange(false);
   }, [setPackages]);
+
+  const addClass = useCallback(
+    (stereoType: StereoType) => {
+      backupSnapshot();
+      const newClass = createNewClass(stereoType, pkg.uuid);
+      setClasses((classes) => [...classes, newClass]);
+      onVisibleChange(false);
+    },
+    [backupSnapshot, createNewClass, onVisibleChange, setClasses]
+  );
+
+  const handleAddDiagram = useCallback(
+    () => {
+      backupSnapshot();
+      const newDiagram = createNewDiagram();
+      setSelectedDiagram(newDiagram.uuid);
+      onVisibleChange(false);
+    },
+    [backupSnapshot, createNewDiagram, onVisibleChange, setSelectedDiagram]
+  );
 
   const menu = useMemo(() => (
     <div style={{ backgroundColor: "#000" }}>
@@ -33,6 +64,10 @@ const PackageAction = memo((
             icon: <FileAddOutlined />,
             label: getLocalMessage("model.AddDiagram"),
             key: '0',
+            onClick: e=>{
+              e.domEvent.stopPropagation();
+              handleAddDiagram();
+            }
           },
           {
             icon: <PlusSquareOutlined />,
@@ -90,7 +125,7 @@ const PackageAction = memo((
   return (
     <Dropdown
       overlay={menu}
-      onVisibleChange = {onVisibleChange}
+      onVisibleChange={onVisibleChange}
       trigger={['click']}
     >
       <Button className='no-border' shape='circle' size='small' onClick={e => e.stopPropagation()}>
