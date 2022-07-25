7528ff2c158ec066518c4d1b4edacb166fa0d034
Water.Li <lizongbin@hotmail.com>
2022-07-25 09:02:50 +0800

    Update ModelContent.tsx

--- packages/studio/src/ModelBoard/ModelContent.tsx
+++ packages/studio/src/ModelBoard/ModelContent.tsx
@@ -13,62 +13,63 @@ import {
 import { PropertyBox } from "./PropertyBox";
 import { usePublishedMeta } from "./hooks/usePublishedMeta";
 import { ID } from "../shared";
+import { useShowError } from "../hooks/useShowError";
+import React from "react";
 
 export const ModelContent = memo(
   (props: { appId?:ID; graph?: Graph; onSetGraph: (graph?: Graph) => void }) => {
-    const { graph, onSetGraph } = props;
+    const { appId, graph, onSetGraph } = props;
     const setPublishedId = useSetRecoilState(publishedIdState(appId));
     const selectedDiagram = useRecoilValue(selectedDiagramState(appId));
     const minMap = useRecoilValue(minMapState(appId));
 
     const { meta, error } = usePublishedMeta();
-    useShowServerError(error);
+    useShowError(error);
     useEffect(() => {
       setPublishedId(meta?.id || undefined);
     }, [meta?.id, setPublishedId]);
 
     return (
-      <Box
-        sx={{
+      <div
+        style={{
           flex: 1,
           display: "flex",
           flexFlow: "column",
         }}
       >
         <ModelToolbar />
-        <Box sx={{ width: "100%", flex: 1, display: "flex", height: "0" }}>
+        <div style={{ width: "100%", flex: 1, display: "flex", height: "0" }}>
           {selectedDiagram ? (
             <>
               <Toolbox graph={graph}></Toolbox>
-              <Box
-                sx={{
+              <div
+                style={{
                   flex: 1,
                   display: "flex",
-                  ...scrollStyles,
                 }}
               >
-                <Box
-                  sx={{
+                <div
+                  style={{
                     flex: 1,
                     display: "flex",
                     flexFlow: "column",
                     overflow: "auto",
                     position: "relative",
-                    "& .x6-widget-minimap": {
-                      backgroundColor: (theme) =>
-                        theme.palette.background.paper,
-                      "& .x6-graph": {
-                        boxShadow: (theme) => theme.shadows[0],
-                      },
-                    },
+                    // "& .x6-widget-minimap": {
+                    //   backgroundColor: (theme) =>
+                    //     theme.palette.background.paper,
+                    //   "& .x6-graph": {
+                    //     boxShadow: (theme) => theme.shadows[0],
+                    //   },
+                    // },
                   }}
                 >
                   <GraphCanvas
                     graph={graph}
                     onSetGraph={onSetGraph}
                   ></GraphCanvas>
-                  <Box
-                    sx={{
+                  <div
+                    style={{
                       position: "absolute",
                       zIndex: 1,
                       bottom: 3,
@@ -78,20 +79,20 @@ export const ModelContent = memo(
                       borderRadius: "5px",
                       overflow: "hidden",
                       display: minMap ? "block" : "none",
-                      border: (theme) => `solid 2px ${theme.palette.divider}`,
-                      boxShadow: 5,
+                      // border: (theme) => `solid 2px ${theme.palette.divider}`,
+                      //boxShadow: 5,
                     }}
                     id="mini-map"
-                  ></Box>
-                </Box>
-              </Box>
+                  ></div>
+                </div>
+              </div>
             </>
           ) : (
             <EmpertyCanvas></EmpertyCanvas>
           )}
           <PropertyBox></PropertyBox>
-        </Box>
-      </Box>
+        </div>
+      </div>
     );
   }
 );
