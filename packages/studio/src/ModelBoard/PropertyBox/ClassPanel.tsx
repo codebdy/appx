f939c3182286c6986e9bfdb72e3bf34fad4a23ec
Water.Li <lizongbin@hotmail.com>
2022-07-26 12:49:33 +0800

    Update ClassPanel.tsx

--- packages/studio/src/ModelBoard/PropertyBox/ClassPanel.tsx
+++ packages/studio/src/ModelBoard/PropertyBox/ClassPanel.tsx
@@ -9,44 +9,6 @@ export const ClassPanel = (props: { cls: ClassMeta }) => {
   const { cls } = props;
   const serviceId = useSelectedAppId();
   const changeClass = useChangeClass(serviceId);
-
-  const handleNameChange = useCallback(
-    (event: React.ChangeEvent<{ value: string }>) => {
-      const name = event.target.value.trim();
-      changeClass({ ...cls, name: name });
-    },
-    [changeClass, cls]
-  );
-
-  const handlePartialNameChange = useCallback(
-    (event: React.ChangeEvent<{ value: string }>) => {
-      const partialName = event.target.value.trim();
-      changeClass({ ...cls, partialName: partialName });
-    },
-    [changeClass, cls]
-  );
-
-  const handleRootChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      changeClass({ ...cls, root: event.target.checked });
-    },
-    [changeClass, cls]
-  );
-
-  // const handleGqlInterfaceChange = useCallback(
-  //   (event: React.ChangeEvent<HTMLInputElement>) => {
-  //     changeClass({ ...cls, gqlInterface: event.target.checked });
-  //   },
-  //   [changeClass, cls]
-  // );
-
-  const handleDescriptionChange = useCallback(
-    (event: React.ChangeEvent<{ value: string }>) => {
-      changeClass({ ...cls, description: event.target.value });
-    },
-    [changeClass, cls]
-  );
-
   const handleChange = useCallback((form) => {
     changeClass({ ...cls, ...form });
   }, [changeClass, cls])
