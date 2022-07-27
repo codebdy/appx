d37215b54527e89f934237984551edb36d81caf1
Water.Li <lizongbin@hotmail.com>
2022-07-27 09:49:55 +0800

    Update index.tsx

--- packages/studio/src/ModelBoard/EntityTree/index.tsx
+++ packages/studio/src/ModelBoard/EntityTree/index.tsx
@@ -26,6 +26,7 @@ import { useParseRelationUuid } from "../hooks/useParseRelationUuid";
 import { useGetSourceRelations } from './../hooks/useGetSourceRelations';
 import { useGetTargetRelations } from './../hooks/useGetTargetRelations';
 import { useGetClass } from "../hooks/useGetClass";
+import { MethodMeta } from "../meta/MethodMeta";
 const { DirectoryTree } = Tree;
 
 export const EntityTree = memo((props: { graph?: Graph }) => {
@@ -139,12 +140,40 @@ export const EntityTree = memo((props: { graph?: Graph }) => {
     }
   }, [getSourceRelations, getTargetRelations])
 
+  const getMethodNode = useCallback((method: MethodMeta) => {
+    return {
+      icon: <SvgIcon>
+        <svg style={{ width: "12px", height: "12px" }} viewBox="0 0 24 24" fill="currentColor">
+          <path fill="currentColor" d="M16 7V3H14V7H10V3H8V7C7 7 6 8 6 9V14.5L9.5 18V21H14.5V18L18 14.5V9C18 8 17 7 16 7M16 13.67L13.09 16.59L12.67 17H11.33L10.92 16.59L8 13.67V9.09C8 9.06 8.06 9 8.09 9H15.92C15.95 9 16 9.06 16 9.09V13.67Z" />
+        </svg>
+      </SvgIcon>,
+      title:
+        <TreeNodeLabel
+          action={
+            <Button
+              type="text"
+              shape="circle"
+              size="small"
+              onClick={(e) => {
+                e.stopPropagation()
+                //removeAttribute(method.uuid);
+              }}
+            >
+              <DeleteOutlined />
+            </Button>
+          }
+        >
+          {method.name}
+        </TreeNodeLabel>,
+      key: method.uuid,
+      isLeaf: true,
+    }
+  }, [removeAttribute]);
   const getClassMethodsNode = useCallback((cls: ClassMeta) => {
-    const children = [];
     return {
       title: getLocalMessage("model.Methods"),
       key: cls.uuid + "methods",
-      children: children,
+      children: cls.methods?.map(method => getMethodNode(method)),
     }
   }, [])
 
