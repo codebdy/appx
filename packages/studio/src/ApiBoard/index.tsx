f85c7affd7eddb8d631178320718776e9beaf543
Water.Li <lizongbin@hotmail.com>
2022-07-27 14:40:43 +0800

    backup

--- packages/studio/src/ApiBoard/index.tsx
+++ packages/studio/src/ApiBoard/index.tsx
@@ -1,6 +1,5 @@
 import GraphiQL from "graphiql";
 import "graphiql/graphiql.css";
-import "./graphiql-dark-mode.css";
 import { createGraphiQLFetcher } from "@graphiql/toolkit";
 import { memo, useMemo } from "react";
 import { SubscriptionClient } from "subscriptions-transport-ws";
@@ -9,6 +8,7 @@ import { useToken } from "@appx/enthooks";
 import { HEADER_AUTHORIZATION, HEADER_APPX_APP_ID, SERVER_SUBSCRIPTION_URL, SERVER_URL, TOKEN_PREFIX } from "../consts";
 import { useSelectedAppId } from './../ModelBoard/hooks/useSelectedAppId';
 import { NON_APP_ID } from "../ModelBoard/recoil/atoms";
+import "./index.less";
 
 //例子連接
 //https://github.com/graphql/graphiql/blob/main/packages/graphiql-toolkit/docs/create-fetcher.md#subscriptionurl
@@ -19,26 +19,22 @@ const ApiBoard = memo(() => {
     const fetcher = createGraphiQLFetcher({
       url: SERVER_URL,
       legacyWsClient: new SubscriptionClient(SERVER_SUBSCRIPTION_URL),
-      headers:{
-        [HEADER_AUTHORIZATION]: token ? `${TOKEN_PREFIX}${token}` :"",
+      headers: {
+        [HEADER_AUTHORIZATION]: token ? `${TOKEN_PREFIX}${token}` : "",
         [HEADER_APPX_APP_ID]: NON_APP_ID !== appId ? appId : undefined,
       }
     });
 
     return fetcher;
   }, [appId, token]);
-  
+
   return (
-    <div
-      style={{
-        height: "100%",
-      }}
-    >
+    <div className="api-board">
       {fetcher && (
         <GraphiQL
           headerEditorEnabled
           fetcher={fetcher}
-          // query=""
+        // query=""
         />
       )}
     </div>
