fe4eed918c3f5de17a4563db16896b2eee7e8eec
Water.Li <lizongbin@hotmail.com>
2022-07-27 14:22:00 +0800

    backup

--- packages/studio/src/ApiBoard/index.tsx
+++ packages/studio/src/ApiBoard/index.tsx
@@ -1,12 +1,48 @@
-import React from "react"
-import { memo } from "react"
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.css";
+import "./graphiql-dark-mode.css";
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
+import { memo, useMemo } from "react";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+import React from "react";
+import { useToken } from "@appx/enthooks";
+import { HEADER_AUTHORIZATION, HEADER_APPX_APP_ID, SERVER_SUBSCRIPTION_URL, SERVER_URL, TOKEN_PREFIX } from "../consts";
+import { useSelectedAppId } from './../ModelBoard/hooks/useSelectedAppId';
+import { NON_APP_ID } from "../ModelBoard/recoil/atoms";
 
+//例子連接
+//https://github.com/graphql/graphiql/blob/main/packages/graphiql-toolkit/docs/create-fetcher.md#subscriptionurl
 const ApiBoard = memo(() => {
-  return (
-    <div>
-      ApiBoard
-    </div>
-  )
-})
+  const appId = useSelectedAppId();
+  const token = useToken();
+  const fetcher = useMemo(() => {
+    const fetcher = createGraphiQLFetcher({
+      url: SERVER_URL,
+      legacyWsClient: new SubscriptionClient(SERVER_SUBSCRIPTION_URL),
+      headers:{
+        [HEADER_AUTHORIZATION]: token ? `${TOKEN_PREFIX}${token}` :"",
+        [HEADER_APPX_APP_ID]: NON_APP_ID !== appId ? appId : undefined,
+      }
+    });
 
-export default ApiBoard
+    return fetcher;
+  }, [appId, token]);
+  
+  return (
+    <div
+      style={{
+        height: "100%",
+      }}
+    >
+      {fetcher && (
+        <GraphiQL
+          headerEditorEnabled
+          fetcher={fetcher}
+          // query=""
+        />
+      )}
+    </div>
+  );
+});
+
+export default ApiBoard;
--- packages/studio/src/consts.ts
+++ packages/studio/src/consts.ts
@@ -1,3 +1,12 @@
 export const SERVER_URL = "http://localhost:4000/graphql"
 
 export const PRIMARY_COLOR = "#5d78ff";
+export const SERVER_SUBSCRIPTION_URL = "ws://localhost:8080/subscriptions";
+export const UPLOAD_SERVER = "http://localhost:4000";
+
+export const HEADER_AUTHORIZATION = "Authorization"
+export const TOKEN_PREFIX= "Bearer "
+export const HEADER_APPX_APP_ID ="APPX_APP_ID"
+
+//普通角色的ID永远不会是1
+export const GUEST_ROLE_ID = "1"
