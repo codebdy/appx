8e9f6c1bccf4b6ab982e6fa3c50d3a05fc8940c3
Water.Li <lizongbin@hotmail.com>
2022-07-28 16:20:17 +0800

    backup

--- packages/studio/src/App.tsx
+++ packages/studio/src/App.tsx
@@ -5,39 +5,33 @@ import Login from './Login';
 import AppDesigner from './AppDesigner';
 import AppConfig from './AppConfig/index';
 import { useQuery, EntiRoot } from '@appx/enthooks';
-import { SERVER_URL } from './consts';
 import ModelBoard from './ModelBoard';
 import AppsContent from './AppManager/AppsContent';
 import ApiBoard from './ApiBoard';
 import AuthBoard from './AuthBoard';
 import { AppManagerRoutes } from './AppManager/AppHeader';
-import { RecoilRoot } from 'recoil';
 import Install from './Install';
 
 const App = memo(() => {
   useQuery()
   return (
-    <RecoilRoot>
-      <EntiRoot config={{ endpoint: SERVER_URL }} >
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<AppManager />}>
-              <Route
-                path={AppManagerRoutes.Root}
-                element={<AppsContent />}
-              />
-              <Route path={AppManagerRoutes.Model} element={<ModelBoard />} />
-              <Route path={AppManagerRoutes.Api} element={<ApiBoard />} />
-              <Route path={AppManagerRoutes.Auth} element={<AuthBoard />} />
-            </Route>
-            <Route path="/config-app/:appId" element={<AppConfig />} />
-            <Route path="/design-app/:deviceSlug/:appId" element={<AppDesigner />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/install" element={<Install />} />
-          </Routes>
-        </BrowserRouter>
-      </EntiRoot >
-    </RecoilRoot>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppManager />}>
+          <Route
+            path={AppManagerRoutes.Root}
+            element={<AppsContent />}
+          />
+          <Route path={AppManagerRoutes.Model} element={<ModelBoard />} />
+          <Route path={AppManagerRoutes.Api} element={<ApiBoard />} />
+          <Route path={AppManagerRoutes.Auth} element={<AuthBoard />} />
+        </Route>
+        <Route path="/config-app/:appId" element={<AppConfig />} />
+        <Route path="/design-app/:deviceSlug/:appId" element={<AppDesigner />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/install" element={<Install />} />
+      </Routes>
+    </BrowserRouter>
   )
 });
 
--- packages/studio/src/index.tsx
+++ packages/studio/src/index.tsx
@@ -4,5 +4,18 @@ import App from "./App";
 import 'antd/dist/antd.less'
 import './index.less'
 import "./locales"
+import { SERVER_URL } from "./consts";
+import { RecoilRoot } from "recoil";
+import { EntiRoot } from "@appx/enthooks";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const AppRoot = () => {
+  return (
+    <RecoilRoot>
+      <EntiRoot config={{ endpoint: SERVER_URL }} >
+        <App />
+      </EntiRoot>
+    </RecoilRoot>
+  )
+}
+
+ReactDOM.render(<AppRoot />, document.getElementById('root'))
