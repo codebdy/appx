23d3af76debf4dd2f43a3edd38760c7fefcaa40d
Water.Li <lizongbin@hotmail.com>
2022-07-30 07:44:38 +0800

    Update useReadMeta.ts

--- packages/studio/src/ModelBoard/hooks/useReadMeta.ts
+++ packages/studio/src/ModelBoard/hooks/useReadMeta.ts
@@ -3,7 +3,7 @@ import { useMemo, useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import { useQueryOne } from "../../enthooks/hooks/useQueryOne";
 import { Meta } from "../meta/Meta";
-import { metaState, classesState, relationsState, diagramsState, x6NodesState, x6EdgesState } from "../recoil/atoms";
+import { metaState, classesState, relationsState, diagramsState, x6NodesState, x6EdgesState, packagesState } from "../recoil/atoms";
 
 export function useReadMeta(appUuid: string): { error?: Error; loading?: boolean } {
   const setMeta = useSetRecoilState(metaState(appUuid));
@@ -12,6 +12,7 @@ export function useReadMeta(appUuid: string): { error?: Error; loading?: boolean
   const setDiagrams = useSetRecoilState(diagramsState(appUuid));
   const setX6Nodes = useSetRecoilState(x6NodesState(appUuid));
   const setX6Edges = useSetRecoilState(x6EdgesState(appUuid));
+  const setPackages = useSetRecoilState(packagesState(appUuid))
 
   const queryName = useMemo(() => "oneMeta", []);
   const queryGql = useMemo(() => {
@@ -26,26 +27,19 @@ export function useReadMeta(appUuid: string): { error?: Error; loading?: boolean
   `;
   }, [queryName]);
   const { data, error, loading } = useQueryOne<Meta>(queryGql);
+
   useEffect(() => {
     if (data) {
       const meta = data[queryName];
       setMeta(meta);
+      setPackages(meta?.content?.packages || []);
       setEntities(meta?.content?.classes || []);
       setRelations(meta?.content?.relations || []);
       setDiagrams(meta?.content?.diagrams || []);
       setX6Nodes(meta?.content?.x6Nodes || []);
       setX6Edges(meta?.content?.x6Edges || []);
     }
-  }, [
-    data,
-    queryName,
-    setDiagrams,
-    setEntities,
-    setMeta,
-    setRelations,
-    setX6Edges,
-    setX6Nodes,
-  ]);
+  }, [data, queryName, setDiagrams, setEntities, setMeta, setPackages, setRelations, setX6Edges, setX6Nodes]);
 
   return { error, loading };
 }
