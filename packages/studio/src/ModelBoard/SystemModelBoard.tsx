61091f8c12d2d18913023a40efc00cf0ed3fa045
Water.Li <lizongbin@hotmail.com>
2022-07-30 17:59:52 +0800

    backup

--- packages/studio/src/ModelBoard/SystemModelBoard.tsx
+++ packages/studio/src/ModelBoard/SystemModelBoard.tsx
@@ -3,11 +3,12 @@ import "@antv/x6-react-shape";
 import "./index.less"
 import ModelsBoard from ".";
 import { SYSTEM_APP_UUID } from "./recoil/atoms";
+import SavaActions from "./SavaActions";
 
 const SystemModelBoard = memo(() => {
   return (
     <div className="system-model-border">
-      <ModelsBoard appUuid={SYSTEM_APP_UUID} />    
+      <ModelsBoard appUuid={SYSTEM_APP_UUID} actions={<SavaActions />} />    
     </div>
 
   );
--- packages/studio/src/ModelBoard/index.tsx
+++ packages/studio/src/ModelBoard/index.tsx
@@ -4,7 +4,6 @@ import { Graph } from "@antv/x6";
 import "@antv/x6-react-shape";
 import { ModelContent } from "./ModelContent";
 import "./index.less"
-import SavaActions from "./SavaActions";
 import { useReadMeta } from "./hooks/useReadMeta";
 import { useShowError } from "../hooks/useShowError";
 import { Spin } from "antd";
@@ -14,9 +13,10 @@ import { selectedAppUuidState } from "./recoil/atoms";
 const ModelsBoard = memo((
   props: {
     appUuid: string,
+    actions?: React.ReactNode,
   }
 ) => {
-  const { appUuid } = props
+  const { appUuid, actions } = props
   const [graph, setGraph] = useState<Graph>();
   //const selectedService = useSelectedService();
   const setAppUuid = useSetRecoilState(selectedAppUuidState);
@@ -35,7 +35,7 @@ const ModelsBoard = memo((
         <div className="model-tree-shell">
           <EntityTree graph={graph}></EntityTree>
         </div>
-        <ModelContent appUuid={appUuid} graph={graph} onSetGraph={setGraph} saveActions={<SavaActions />} />
+        <ModelContent appUuid={appUuid} graph={graph} onSetGraph={setGraph} saveActions={actions} />
       </div>
     </Spin>
   );
