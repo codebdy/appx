07cf1efb20fdca5ed8d54168f1f1b51ebaa73f91
Water.Li <lizongbin@hotmail.com>
2022-07-30 08:24:24 +0800

    Update index.tsx

--- packages/studio/src/ApiBoard/index.tsx
+++ packages/studio/src/ApiBoard/index.tsx
@@ -2,35 +2,34 @@ import GraphiQL from "graphiql";
 import "graphiql/graphiql.css";
 import { createGraphiQLFetcher } from "@graphiql/toolkit";
 import { memo, useMemo } from "react";
-import { SubscriptionClient } from "subscriptions-transport-ws";
 import React from "react";
-import { HEADER_AUTHORIZATION, SERVER_SUBSCRIPTION_URL, SERVER_URL, TOKEN_PREFIX, HEADER_APPX_APPUUID } from "../consts";
+import { HEADER_AUTHORIZATION, TOKEN_PREFIX, HEADER_APPX_APPUUID } from "../consts";
 import { useSelectedAppUuid } from '../ModelBoard/hooks/useSelectedAppUuid';
-import { SYSTEM_APP_UUID } from "../ModelBoard/recoil/atoms";
 import "./index.less";
-import { useToken } from "../enthooks";
+import { useEndpoint, useToken } from "../enthooks";
 
 //例子連接
 //https://github.com/graphql/graphiql/blob/main/packages/graphiql-toolkit/docs/create-fetcher.md#subscriptionurl
 const ApiBoard = memo(() => {
   const appUuid = useSelectedAppUuid();
   const token = useToken();
+  const endppoint = useEndpoint();
   const fetcher = useMemo(() => {
     const fetcher = createGraphiQLFetcher({
-      url: SERVER_URL,
-      legacyWsClient: new SubscriptionClient(SERVER_SUBSCRIPTION_URL),
+      url: endppoint,
+      // legacyWsClient: new SubscriptionClient(SERVER_SUBSCRIPTION_URL),
       headers: {
         [HEADER_AUTHORIZATION]: token ? `${TOKEN_PREFIX}${token}` : "",
-        [HEADER_APPX_APPUUID]: SYSTEM_APP_UUID !== appUuid ? appUuid : undefined,
+        [HEADER_APPX_APPUUID]: appUuid,
       }
     });
 
     return fetcher;
-  }, [appUuid, token]);
+  }, [appUuid, endppoint, token]);
 
   return (
     <div className="api-board">
-      {fetcher && (
+      {fetcher && endppoint && (
         <GraphiQL
           headerEditorEnabled
           fetcher={fetcher}
