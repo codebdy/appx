e7d7b5f5d2d69a24e00210b3ad3ade855aa06c31
Water.Li <lizongbin@hotmail.com>
2022-07-31 18:04:19 +0800

    backup

--- packages/studio/src/AppDesigner/widgets/PageListWidget/FormTemplates.tsx
+++ packages/studio/src/AppDesigner/widgets/PageListWidget/FormTemplates.tsx
@@ -5,22 +5,29 @@ import { memo } from "react";
 const FormTemplates = memo(() => {
 
   return (
-    <Row gutter={16}>
-      <Col span={8}>
-        <Card title="Card title" bordered={false}>
-          Card content
+    <Row className="page-template-list" gutter={16}>
+      <Col span={6}>
+        <Card style={{textAlign: 'center'}}
+        cover={<img alt="example" src="/public/img/blank.png" />}
+        >
+          新建空白页
         </Card>
       </Col>
-      <Col span={8}>
-        <Card title="Card title" bordered={false}>
-          Card content
+      <Col style={{textAlign: 'center', padding:"8px"}} span={6}>
+        <Card
+          cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+        >
+          查询列表
         </Card>
       </Col>
-      <Col span={8}>
-        <Card title="Card title" bordered={false}>
-          Card content
+      <Col style={{textAlign: 'center', padding:"8px"}} span={6}>
+        <Card
+          cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+        >
+          单表格
         </Card>
       </Col>
+
     </Row>
   )
 })
