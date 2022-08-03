1525aed42200f986a7abb2087f712fbf656e0a37
Water.Li <lizongbin@hotmail.com>
2022-08-03 15:28:08 +0800

    backup

---
+++ packages/studio/src/AppDesigner/components/PageContainer/index.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from "react"
+
+const PageContainer = memo(()=>{
+  return (
+    <></>
+  )
+})
+
+export default PageContainer
---
+++ packages/studio/src/AppDesigner/components/ProTable/index.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from "react"
+
+const ProTable = memo(()=>{
+  return (
+    <></>
+  )
+})
+
+export default ProTable
