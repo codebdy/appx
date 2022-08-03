f1a07ef3899420e201e7b616684d36cbf3ae677a
Water.Li <lizongbin@hotmail.com>
2022-08-03 15:48:54 +0800

    backup

--- packages/studio/src/AppRunner/index.tsx
+++ packages/studio/src/AppRunner/index.tsx
@@ -1,12 +1,11 @@
 import React from "react"
 import { memo } from "react"
+import ProLayout from "./ProLayout"
 
 const AppRunner = memo(()=>{
 
   return (
-    <>
-    AppRunner
-    </>
+    <ProLayout />
   )
 })
 
