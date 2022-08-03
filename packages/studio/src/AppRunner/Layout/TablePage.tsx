fb4af1e0ca22a8c009068f417452d08c99bae888
Water.Li <lizongbin@hotmail.com>
2022-08-03 17:39:03 +0800

    backup

--- packages/studio/src/AppRunner/Layout/TablePage.tsx
+++ packages/studio/src/AppRunner/Layout/TablePage.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { PageContainer } from '@ant-design/pro-components';
+import ProTable from "../../components/ProTable";
 
 
 const TablePage = () => {
   return (
     <PageContainer>
-
-
-
+      <ProTable />
     </PageContainer>
   )
 }
--- packages/studio/src/components/ProTable/index.tsx
+++ packages/studio/src/components/ProTable/index.tsx
@@ -1,8 +1,14 @@
+import { Card } from "antd"
 import React, { memo } from "react"
+import "./index.less"
 
 const ProTable = memo(()=>{
   return (
-    <div>ProTable</div>
+    <div className="appx-pro-table">
+      <Card>
+        Card
+      </Card>
+    </div>
   )
 })
 
