e4aea4269b0fdcb40a3a6e46952a221631c167ba
Water.Li <lizongbin@hotmail.com>
2022-08-04 12:57:35 +0800

    Update index.tsx

--- packages/studio/src/components/Table/SelectMessage/index.tsx
+++ packages/studio/src/components/Table/SelectMessage/index.tsx
@@ -1,15 +1,19 @@
-import { Alert, Button } from "antd"
+import { Alert, Button, Space } from "antd"
 import React from "react";
 
 const SelectMessage = () => {
   return (
     <Alert
       message={
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems:"center" }}>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
           <div>
             已选择 1 项
+            <Button type="link">取消选择</Button>
           </div>
-          <Button type="link">取消选择</Button>
+          <Space style={{ display: 'flex', alignItems: 'center' }}>
+            <Button>批量删除</Button>
+            <Button type="primary">批量审批</Button>
+          </Space>
         </div>
       }
       type="info"
