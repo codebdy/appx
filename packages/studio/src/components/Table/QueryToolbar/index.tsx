d9cbb3190dbe5d421535c8b3ab6641afa9b39282
Water.Li <lizongbin@hotmail.com>
2022-08-04 12:02:45 +0800

    Update index.tsx

--- packages/studio/src/components/Table/QueryToolbar/index.tsx
+++ packages/studio/src/components/Table/QueryToolbar/index.tsx
@@ -1,23 +1,33 @@
+import { PlusOutlined } from "@ant-design/icons"
+import { Button, Space } from "antd"
 import React, { memo } from "react"
 
-const QueryToolbar = memo(()=>{
+const QueryToolbar = memo(() => {
   return (
     <div style={{
-      display:"flex",
-      alignItems:"center",
-      justifyContent:"space-between"
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between"
     }}>
       <div>
         查询表格
       </div>
-      <div
+      <Space
         style={{
-          display:"flex",
-          alignItems:"center",
+          display: "flex",
+          alignItems: "center",
         }}
       >
-        ddd
-      </div>
+
+        <Button
+          type="primary"
+          icon = {
+            <PlusOutlined />
+          }
+        >
+          新建
+        </Button>
+      </Space>
     </div>
   )
 })
