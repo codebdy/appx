394a8fe06918689a8ff3b2ae7c5874bad9440501
Water.Li <lizongbin@hotmail.com>
2022-08-04 11:44:02 +0800

    backup

--- packages/studio/src/components/Table/QueryTable/index.tsx
+++ packages/studio/src/components/Table/QueryTable/index.tsx
@@ -1,11 +1,82 @@
-import React, { memo } from "react"
-import "./index.less"
+import { Table } from 'antd';
+import React from 'react';
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Chinese Score',
+    dataIndex: 'chinese',
+    sorter: {
+      compare: (a, b) => a.chinese - b.chinese,
+      multiple: 3,
+    },
+  },
+  {
+    title: 'Math Score',
+    dataIndex: 'math',
+    sorter: {
+      compare: (a, b) => a.math - b.math,
+      multiple: 2,
+    },
+  },
+  {
+    title: 'English Score',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    chinese: 98,
+    math: 60,
+    english: 70,
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    chinese: 98,
+    math: 66,
+    english: 89,
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    chinese: 98,
+    math: 90,
+    english: 70,
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    chinese: 88,
+    math: 99,
+    english: 89,
+  },
+];
 
-const QueryTable = memo(()=>{
-  return (
-    <div>
-    </div>
+const onChange = (pagination, filters, sorter, extra) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
+
+const QueryTable = () => {
+  return (<Table
+    columns={columns}
+    title={
+      () => {
+        return (<div>Tool bar</div>)
+      }
+    }
+    dataSource={data}
+    onChange={onChange} />
   )
-})
 
-export default QueryTable
+};
+
+export default QueryTable;
--- packages/studio/src/components/Table/index.tsx
+++ packages/studio/src/components/Table/index.tsx
@@ -2,12 +2,16 @@ import { Card } from "antd"
 import React, { memo } from "react"
 import "./index.less"
 import { QueryFormExample } from "./QueryForm"
+import QueryTable from "./QueryTable"
 
-const ProTable = memo(()=>{
+const ProTable = memo(() => {
   return (
     <div className="appx-pro-table">
       <Card>
         <QueryFormExample />
+      </Card>
+      <Card style={{ marginTop: "16px" }}>
+        <QueryTable />
       </Card>
     </div>
   )
