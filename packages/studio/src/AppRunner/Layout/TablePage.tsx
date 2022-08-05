c7b258d78d5418e927706d821a0878edec8299d1
Water.Li <lizongbin@hotmail.com>
2022-08-05 16:59:23 +0800

    Update TablePage.tsx

--- packages/studio/src/AppRunner/Layout/TablePage.tsx
+++ packages/studio/src/AppRunner/Layout/TablePage.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { PageContainer } from '@ant-design/pro-components';
 import ProTable from "../../components/ProTable";
+import { createForm } from '@formily/core'
+import { FormProvider, createSchemaField } from '@formily/react'
+import { FormItem } from "@formily/antd";
 
+const SchemaField = createSchemaField({
+  components: {
+    ProTable,
+    FormItem
+  },
+})
+
+const form = createForm()
 
 const TablePage = () => {
   return (
     <PageContainer>
-      <ProTable />
+      <FormProvider form={form}>
+        <SchemaField>
+          <SchemaField.Array
+            name="array"
+            x-decorator="FormItem"
+            x-component="ProTable"
+            x-component-props={{
+              pagination: { pageSize: 10 },
+              scroll: { x: '100%' },
+            }}
+          >
+          </SchemaField.Array>
+        </SchemaField>
+      </FormProvider>
     </PageContainer>
   )
 }
