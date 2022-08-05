19d65e4f2bf85ae6f5cc838c1646dcfbf19e4a96
Water.Li <lizongbin@hotmail.com>
2022-08-05 11:50:41 +0800

    backup

--- packages/studio/src/i18n/useResourceBoundles.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect } from "react";
-
-export function useResourceBoundles() {
-  useEffect(() => {
-
-  }, []);
-}
---
+++ packages/studio/src/i18n/useResourceBundles.ts
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
+export function useResourceBundles(nameSpace: string, bundles: { [key: string]: any }) {
+  const { i18n } = useTranslation();
+  useEffect(() => {
+    for (const key of Object.keys(bundles)) {
+      if (!i18n.hasResourceBundle(key, nameSpace)) {
+        i18n.addResourceBundle(key, nameSpace, bundles[key])
+      }
+    };
+
+  }, [bundles, i18n, nameSpace]);
+}
