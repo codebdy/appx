ec644acbfeeee1166556218b58de8cd6c8db4c8f
Water.Li <lizongbin@hotmail.com>
2022-08-07 14:28:46 +0800

    Revert "backup"
    This reverts commit 9a8c98fcfcb6507557a6686e548d2fe206b282f5.

--- packages/studio/src/components/Field/shared.ts
+++ packages/studio/src/components/Field/shared.ts
@@ -74,62 +74,6 @@ export const createComponentSchemaTab = (
           },
           properties: {
             'x-component-props': component,
-            name: {
-              type: 'string',
-              'x-decorator': 'FormItem',
-              'x-component': 'Input',
-            },
-            title: {
-              type: 'string',
-              'x-decorator': 'FormItem',
-              'x-component': 'Input',
-              'x-reactions': {
-                fulfill: {
-                  state: {
-                    hidden: '{{$form.values["x-decorator"] !== "FormItem"}}',
-                  },
-                },
-              },
-            },
-            description: {
-              type: 'string',
-              'x-decorator': 'FormItem',
-              'x-component': 'Input.TextArea',
-              'x-reactions': {
-                fulfill: {
-                  state: {
-                    hidden: '{{$form.values["x-decorator"] !== "FormItem"}}',
-                  },
-                },
-              },
-            },
-            'x-display': {
-              type: 'string',
-              enum: ['visible', 'hidden', 'none', ''],
-              'x-decorator': 'FormItem',
-              'x-component': 'Select',
-              'x-component-props': {
-                defaultValue: 'visible',
-              },
-            },
-            'x-pattern': {
-              type: 'string',
-              enum: ['editable', 'disabled', 'readOnly', 'readPretty', ''],
-              'x-decorator': 'FormItem',
-              'x-component': 'Select',
-              'x-component-props': {
-                defaultValue: 'editable',
-              },
-            },
-            'x-reactions': {
-              'x-decorator': 'FormItem',
-              'x-component': ReactionsSetter,
-            },
-            // 'x-decorator': {
-            //   type: 'string',
-            //   'x-decorator': 'FormItem',
-            //   'x-component': FormItemSwitcher,
-            // },
           },
         },
         'decorator-group': decorator && {
