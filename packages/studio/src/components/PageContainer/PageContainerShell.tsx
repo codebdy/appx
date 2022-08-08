ba6f3ef229908f75a8693bd0cab11b3c0b813c0d
Water.Li <lizongbin@hotmail.com>
2022-08-08 14:44:33 +0800

    backup

---
+++ packages/studio/src/components/PageContainer/PageContainerShell.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import clx from 'classnames'
+import { IPageContainerProps } from "./IPageContainerProps"
+
+export const PageContainerShell = (props: IPageContainerProps) => {
+  const { children, className, ...other } = props
+  return (
+    <div className={clx(className, 'rx_page_container')} {...other}>{children}</div>
+  )
+}
