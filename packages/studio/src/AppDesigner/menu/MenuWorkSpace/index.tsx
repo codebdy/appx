590f710ae9e5b43c0b46b3c66669d66ca0968d64
Water.Li <lizongbin@hotmail.com>
2022-08-09 11:00:30 +0800

    Update index.tsx

--- packages/studio/src/AppDesigner/menu/MenuWorkSpace/index.tsx
+++ packages/studio/src/AppDesigner/menu/MenuWorkSpace/index.tsx
@@ -1,16 +1,23 @@
 import React, { memo } from "react";
+import { useTranslation } from "react-i18next";
 import { Workspace } from "../../containers";
-import { ViewportPanel, WorkspacePanel } from "../../panels";
+import { SettingsPanel, ViewportPanel, WorkspacePanel } from "../../panels";
 
-const MenuWorkSpace = memo(()=>{
+const MenuWorkSpace = memo(() => {
+  const { t } = useTranslation();
   return (
-    <Workspace id="menu">
-    <WorkspacePanel>
-      <ViewportPanel style={{ height: '100%' }}>
-        呵呵呵
-      </ViewportPanel>
-    </WorkspacePanel>
-  </Workspace>
+    <>
+      <Workspace id="menu">
+        <WorkspacePanel>
+          <ViewportPanel style={{ height: '100%' }}>
+            呵呵呵
+          </ViewportPanel>
+        </WorkspacePanel>
+      </Workspace>
+      <SettingsPanel title={t("Panels.PropertySettings")}>
+        哈哈哈
+      </SettingsPanel>
+    </>
   )
 })
 
