4760d591fc3baee85178b9c59b87f2b411c870b5
Water.Li <lizongbin@hotmail.com>
2022-08-10 18:49:26 +0800

    Update useUndo.ts

--- packages/studio/src/AppDesigner/menu/hooks/useUndo.ts
+++ packages/studio/src/AppDesigner/menu/hooks/useUndo.ts
@@ -1,3 +1,19 @@
-export function useUndo(){
-  
+import { useCallback } from "react";
+import { useRecoilState } from "recoil";
+import { useDesingerKey } from "../../context"
+import { redoListState, undoListState } from "../atoms";
+
+export function useUndo() {
+  const key = useDesingerKey();
+  const [undoList, setUndoList] = useRecoilState(undoListState(key));
+  const [reddoList, setRedoList] = useRecoilState(redoListState(key));
+
+  const undo = useCallback(() => {
+
+  }, []);
+
+  return {
+    undoDisabled: undoList.length === 0,
+    undo
+  }
 }
