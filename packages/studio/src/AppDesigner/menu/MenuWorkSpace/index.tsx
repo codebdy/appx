20e7fb43f52f64dcd7b3830f2b422845924eed4b
Water.Li <lizongbin@hotmail.com>
2022-08-10 12:28:17 +0800

    Update index.tsx

--- packages/studio/src/AppDesigner/menu/MenuWorkSpace/index.tsx
+++ packages/studio/src/AppDesigner/menu/MenuWorkSpace/index.tsx
@@ -1,17 +1,12 @@
 import React, { memo } from "react";
 import { useTranslation } from "react-i18next";
-import { useRecoilValue } from "recoil";
 import { Workspace } from "../../containers";
-import { useDesingerKey } from "../../context";
 import { SettingsPanel, ToolbarPanel, ViewportPanel, WorkspacePanel } from "../../panels";
-import { navigationRootNodeState } from "../atoms";
 import { MenuToolsWidget } from "../MenuToolsWidget";
 import MenuDesignView from "./MenuDesignView";
 import "./style.less"
 
 const MenuWorkSpace = memo(() => {
-  const key = useDesingerKey();
-  const rootNode = useRecoilValue(navigationRootNodeState(key));
   const { t } = useTranslation();
 
   return (
@@ -23,11 +18,7 @@ const MenuWorkSpace = memo(() => {
           </ToolbarPanel>
           <ViewportPanel style={{ height: '100%' }}>
             <div className="menu-design-view-container">
-              {
-                rootNode &&
-                <MenuDesignView/>
-              }
-
+              <MenuDesignView />
             </div>
           </ViewportPanel>
         </WorkspacePanel>
