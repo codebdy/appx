8adb309a0289cf134d9d8e96ee5d61a1adb098d8
Water.Li <lizongbin@hotmail.com>
2022-08-11 13:35:12 +0800

    Update index.tsx

--- packages/studio/src/shared/icon/IconInput/index.tsx
+++ packages/studio/src/shared/icon/IconInput/index.tsx
@@ -1,12 +1,31 @@
-import { StarOutlined } from "@ant-design/icons";
-import { Button } from "antd";
-import React from "react";
+import { BorderOutlined, CloseCircleFilled, StarOutlined } from "@ant-design/icons";
+import { Badge, Button } from "antd";
+import React, { useCallback } from "react";
 import { memo } from "react"
 
 const IconInput = memo(() => {
+  const handelRemove = useCallback(() => {
+    console.log("哈哈哈shandiao ")
+  }, []);
+
   return (
-    <div>
-      <Button icon={<StarOutlined />} />
+    <div style={{
+      display: "flex",
+      alignItems: "center"
+    }}>
+      <Badge
+        count={
+          <Button
+            icon={<CloseCircleFilled style={{ color: "grey" }} />}
+            type="text"
+            size="small"
+            onClick={handelRemove}
+          />
+        }
+      >
+        <Button icon={<BorderOutlined style={{ color: "transparent" }} />} />
+      </Badge>
+
     </div>
   )
 })
