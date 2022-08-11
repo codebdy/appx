3ba1cd3ee6c227c567670b3d8a9dd6be79614cf8
Water.Li <lizongbin@hotmail.com>
2022-08-11 17:18:16 +0800

    backup

--- packages/studio/src/AppDesigner/menu/MenuWorkSpace/MenuSettingsForm/index.tsx
+++ packages/studio/src/AppDesigner/menu/MenuWorkSpace/MenuSettingsForm/index.tsx
@@ -1,4 +1,4 @@
-import { Form, Input } from 'antd';
+import { Form, Input, Select } from 'antd';
 import IconInput from '../../../../shared/icon/IconInput';
 import React, { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -8,6 +8,16 @@ import { useMenuNode } from '../../hooks/useMenuNode';
 import { useSetMeta } from '../../hooks/useSetMeta';
 import { useDesingerKey } from '../../../context';
 import { MenuItemType } from '../../models/IMenuNode';
+const { Option } = Select;
+
+const children: React.ReactNode[] = [];
+for (let i = 10; i < 36; i++) {
+  children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+}
+
+const handleAuthPointsChange = (value: string[]) => {
+  console.log(`selected ${value}`);
+};
 
 const MenuSettingsForm = memo(() => {
   const { t } = useTranslation();
@@ -57,7 +67,21 @@ const MenuSettingsForm = memo(() => {
             </>
             : <div>{t("Menu.Divider")}</div>
         }
-
+        <Form.Item
+          label={t("AuthPoints")}
+          name="authPoints"
+        >
+          <Select
+            mode="multiple"
+            allowClear
+            style={{ width: '100%' }}
+            placeholder="Please select"
+            defaultValue={['a10', 'c12']}
+            onChange={handleAuthPointsChange}
+          >
+            {children}
+          </Select>
+        </Form.Item>
       </Form>
     </div>
   );
