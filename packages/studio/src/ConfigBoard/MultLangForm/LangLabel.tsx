d1ad8d675e984d5c49bf0700cf9d63c5907edb65
Water.Li <lizongbin@hotmail.com>
2022-08-13 20:43:02 +0800

    Update LangLabel.tsx

--- packages/studio/src/ConfigBoard/MultLangForm/LangLabel.tsx
+++ packages/studio/src/ConfigBoard/MultLangForm/LangLabel.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { memo } from "react";
 import { useTranslation } from "react-i18next";
 import { ILang } from "../../model";
 
-const LangLabel = memo((
+const LangLabel = React.forwardRef((
   props: {
     lang: ILang
-  }
+  },
+  ref: any
 ) => {
 
   const { lang } = props;
   const { t } = useTranslation();
 
   return (
-    <div className="lang-item">
+    <div ref={ref} className="lang-item">
       <div className="lang-abbr">
         {lang.abbr.toUpperCase()}
       </div>
