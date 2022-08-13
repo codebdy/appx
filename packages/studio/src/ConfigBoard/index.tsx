405223dddb09694bd479874c11786396c65a3021
Water.Li <lizongbin@hotmail.com>
2022-08-13 12:38:52 +0800

    backup

--- packages/studio/src/ConfigBoard/index.tsx
+++ packages/studio/src/ConfigBoard/index.tsx
@@ -1,14 +1,39 @@
-import { Switch } from "antd";
-import React, { memo } from "react";
+import { Collapse, Switch } from 'antd';
+import React, { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+const { Panel } = Collapse;
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
 
 const ConfigBoard = memo(() => {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      多语言
-      <Switch defaultChecked />
-      多语言资源
+    <div
+      style={{ display: "flex", justifyContent: "center" }}
+    >
+      <div
+        style={{
+          width: 800,
+          marginTop: 16,
+        }}
+      >
+        <Collapse defaultActiveKey={['muti-lang']}>
+          <Panel header={t("Config.MultiLang")} key="muti-lang">
+            多语言
+            <Switch defaultChecked />
+            多语言资源
+          </Panel>
+          <Panel header={t("Config.Other")} key="other">
+            <p>{text}</p>
+          </Panel>
+        </Collapse>
+      </div>
     </div>
-  )
-})
+  );
+});
 
 export default ConfigBoard;
