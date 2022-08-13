0b92e702992284f960a13e4d32a0bcdc43a3aa16
Water.Li <lizongbin@hotmail.com>
2022-08-13 17:31:47 +0800

    backup

--- packages/studio/src/ConfigBoard/MultLangForm/LangSelect.tsx
+++ packages/studio/src/ConfigBoard/MultLangForm/LangSelect.tsx
@@ -2,9 +2,11 @@ import { FormOutlined } from "@ant-design/icons";
 import { Button, Modal, Tag } from "antd";
 import React, { useCallback, useState } from "react";
 import { memo } from "react";
+import { useTranslation } from "react-i18next";
 
 const LangSelect = memo(() => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const { t } = useTranslation();
   const showModal = useCallback(() => {
     setIsModalVisible(true);
   }, []);
@@ -28,9 +30,11 @@ const LangSelect = memo(() => {
         </div>
       </Button>
       <Modal
-        title="Basic Modal"
+        title={t("Config.MultiLang.LangsEdit")}
         width={700}
         visible={isModalVisible}
+        okText={t("Confirm")}
+        cancelText={t("Cancel")}
         onOk={handleOk}
         onCancel={handleCancel}
       >
