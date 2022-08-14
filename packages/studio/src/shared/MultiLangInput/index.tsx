2b46d5cc5c1ae0d3a40ca9d70349c60c2f3a9afe
Water.Li <lizongbin@hotmail.com>
2022-08-14 20:38:20 +0800

    Update index.tsx

--- packages/studio/src/shared/MultiLangInput/index.tsx
+++ packages/studio/src/shared/MultiLangInput/index.tsx
@@ -1,6 +1,7 @@
 import { TranslationOutlined } from "@ant-design/icons";
 import { Button, Input } from "antd";
 import React, { useCallback, useMemo, useState } from "react";
+import { useParseLangMessage } from "../../hooks/useParseLangMessage";
 import { useAppConfig } from "../AppRoot/context";
 
 const MultiLangInput = (
@@ -14,6 +15,8 @@ const MultiLangInput = (
   const appConfig = useAppConfig();
   const [visiable, setVisiable] = useState(false);
 
+  const parse = useParseLangMessage();
+
   const handleClose = useCallback(() => {
     setVisiable(false)
   }, [])
@@ -22,9 +25,11 @@ const MultiLangInput = (
 
   const isMultLang = appConfig?.schemaJson?.multiLang?.open;
 
+  const parsedValue = useMemo(() => parse(value), [parse, value]);
+
   return (
     <Input.Group compact>
-      <InputCtrl style={{ width: isMultLang ? 'calc(100% - 32px)' : "100%" }} value={value} onChange={onChange} />
+      <InputCtrl style={{ width: isMultLang ? 'calc(100% - 32px)' : "100%" }} value={parsedValue} onChange={onChange} />
       {
         isMultLang &&
         <Button icon={<TranslationOutlined />} style={{ width: "32px" }}></Button>
