09935d74d9968c3dc844f1813f2bb5f4923acac6
Water.Li <lizongbin@hotmail.com>
2022-08-16 12:30:16 +0800

    backup

--- packages/studio/src/SettingsForm/components/DataSourceInput/index.tsx
+++ packages/studio/src/SettingsForm/components/DataSourceInput/index.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import { FormOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import React, { memo } from "react";
+import { useTranslation } from "react-i18next";
 
-const DataSourceInput = () => {
+const DataSourceInput = memo((
+  props: {
+
+  }
+) => {
+  const {t} = useTranslation();
   return (
-    <div>
-      哈哈
-    </div>
+    <>
+      <Button
+        block
+      >
+        {t("SettingsForm.ConfigDataSource")}
+      </Button>
+    </>
   )
-}
+})
 
 export default DataSourceInput;
