92a0550028847c50393d5c8bb0aa7e5118e807aa
Water.Li <lizongbin@hotmail.com>
2022-08-16 13:36:10 +0800

    backup

--- packages/studio/src/SettingsForm/components/DataSourceInput/index.tsx
+++ packages/studio/src/SettingsForm/components/DataSourceInput/index.tsx
@@ -1,7 +1,8 @@
-import { Button, Modal } from "antd";
+import { Button, Form, Input, Modal } from "antd";
 import React, { memo, useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { MonacoInput } from '@designable/react-settings-form'
+import "./style.less"
 
 const DataSourceInput = memo((
   props: {
@@ -33,36 +34,43 @@ const DataSourceInput = memo((
       </Button>
       <Modal
         title={t("SettingsForm.ConfigDataSource")}
+        className = "config-datasource-modal"
         visible={isModalVisible}
         okText={t("Confirm")}
         cancelText={t("Cancel")}
         onOk={handleOk}
         onCancel={handleCancel}
       >
-
-        <div>
-          实体
-        </div>
-        <div style={{
-          height: "300px"
-        }}>
-          <MonacoInput
-            //{...props}
-            options={{
-              readOnly: false,
-              glyphMargin: false,
-              folding: false,
-              lineNumbers: "off",
-              lineDecorationsWidth: 0,
-              lineNumbersMinChars: 0,
-              minimap: {
-                enabled: false,
-              }
-            }}
-            //value={transformToMarkupSchemaCode(props.tree)}
-            language="sql"
-          />
-        </div>
+        <Form
+          layout={"vertical"}
+          initialValues={{}}
+        >
+          <Form.Item label="实体">
+            <Input placeholder="input placeholder" />
+          </Form.Item>
+          <Form.Item label="表达式">
+            <div style={{
+              height: "200px"
+            }}>
+              <MonacoInput
+                //{...props}
+                options={{
+                  readOnly: false,
+                  glyphMargin: false,
+                  folding: false,
+                  lineNumbers: "off",
+                  lineDecorationsWidth: 0,
+                  lineNumbersMinChars: 0,
+                  minimap: {
+                    enabled: false,
+                  }
+                }}
+                //value={transformToMarkupSchemaCode(props.tree)}
+                language="sql"
+              />
+            </div>
+          </Form.Item>
+        </Form>
       </Modal>
     </>
   )
