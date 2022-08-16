e600680becae2917a14aaf746494f505c244b66f
Water.Li <lizongbin@hotmail.com>
2022-08-16 12:38:23 +0800

    Update index.tsx

--- packages/studio/src/SettingsForm/components/DataSourceInput/index.tsx
+++ packages/studio/src/SettingsForm/components/DataSourceInput/index.tsx
@@ -1,6 +1,5 @@
-import { FormOutlined } from "@ant-design/icons";
-import { Button } from "antd";
-import React, { memo } from "react";
+import { Button, Modal } from "antd";
+import React, { memo, useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const DataSourceInput = memo((
@@ -8,14 +7,34 @@ const DataSourceInput = memo((
 
   }
 ) => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = useCallback(() => {
+    setIsModalVisible(true);
+  }, []);
+
+  const handleOk = useCallback(() => {
+    setIsModalVisible(false);
+  }, []);
+
+  const handleCancel = useCallback(() => {
+    setIsModalVisible(false);
+  }, []);
+
   return (
     <>
       <Button
         block
+        onClick={showModal}
       >
         {t("SettingsForm.ConfigDataSource")}
       </Button>
+      <Modal title={t("SettingsForm.ConfigDataSource")} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </>
   )
 })
