4d8324c279a3c19fdfb284ac0b65ace886a68ddd
Water.Li <lizongbin@hotmail.com>
2022-08-16 18:50:52 +0800

    backup

--- packages/studio/src/SettingsForm/components/DataSourceInput/index.tsx
+++ packages/studio/src/SettingsForm/components/DataSourceInput/index.tsx
@@ -1,11 +1,9 @@
-import { Button, Form, Modal, Select } from "antd";
+import { Button, Modal } from "antd";
 import React, { memo, useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { MonacoInput } from '@designable/react-settings-form'
 import "./style.less"
 import { useGetPackageEntities, usePackages } from "../../../datasource/hooks";
-
-const { OptGroup, Option } = Select;
 
 const DataSourceInput = memo((
   props: {
@@ -16,16 +14,6 @@ const DataSourceInput = memo((
   const [isModalVisible, setIsModalVisible] = useState(false);
   const packages = usePackages();
   const getPackageEntities = useGetPackageEntities();
-
-  const parser = require('js-sql-parser');
-  const ast = parser.parse('select * from dual');
-  
-  console.log(JSON.stringify(ast, null, 2));
-  
-  ast.value.selectItems.value[0].value = 'foo';
-  ast.value.from.value[0].value.value.value = 'bar';
-  
-  console.log(parser.stringify(ast));
 
   const showModal = useCallback(() => {
     setIsModalVisible(true);
@@ -50,61 +38,35 @@ const DataSourceInput = memo((
       <Modal
         title={t("SettingsForm.ConfigDataSource")}
         className="config-datasource-modal"
-        width={800}
+        width={700}
         visible={isModalVisible}
         okText={t("Confirm")}
         cancelText={t("Cancel")}
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <Form
-          layout={"vertical"}
-          initialValues={{}}
-        >
-          <Form.Item label="实体">
-            <Select
-            //onChange={handleChange}
-            >
-              {
-                packages?.map(pkg => {
-                  return (
-                    <OptGroup key={pkg.uuid} label={pkg.name}>
-                      {
-                        getPackageEntities(pkg.uuid)?.map(entity => {
-                          return (
-                            <Option key={entity.uuid} value={entity.uuid}>{entity.name}</Option>
-                          )
-                        })
-                      }
-                    </OptGroup>
-                  )
-                })
-              }
-            </Select>
-          </Form.Item>
-          <Form.Item label="表达式">
-            <div style={{
-              height: "200px"
-            }}>
-              <MonacoInput
-                //{...props}
-                options={{
-                  readOnly: false,
-                  glyphMargin: false,
-                  folding: false,
-                  lineNumbers: "off",
-                  lineDecorationsWidth: 0,
-                  lineNumbersMinChars: 0,
-                  minimap: {
-                    enabled: false,
-                  }
-                }}
-                //value={transformToMarkupSchemaCode(props.tree)}
-                language="sql"
-              />
-            </div>
-          </Form.Item>
-        </Form>
+        <div className="form-pannel">
+          <div className="expresion-input">
+            <MonacoInput
+              //{...props}
+              options={{
+                readOnly: false,
+                //glyphMargin: false,
+                folding: false,
+                //lineNumbers: "off",
+                lineDecorationsWidth: 0,
+                lineNumbersMinChars: 0,
+                minimap: {
+                  enabled: false,
+                }
+              }}
+              //value={transformToMarkupSchemaCode(props.tree)}
+              language="sql"
+            />
+          </div>
+        </div>
+
+
       </Modal>
     </>
   )
