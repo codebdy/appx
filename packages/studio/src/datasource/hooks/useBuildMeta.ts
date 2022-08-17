d92851542b6d7a378ca7f983ff43a4d974264c10
Water.Li <lizongbin@hotmail.com>
2022-08-17 18:46:48 +0800

    backup

--- packages/studio/src/datasource/hooks/useBuildMeta.ts
+++ packages/studio/src/datasource/hooks/useBuildMeta.ts
@@ -3,10 +3,30 @@ import { useMemo, useEffect, useCallback } from "react";
 import { useSetRecoilState } from "recoil";
 import { SYSTEM_APP_UUID } from "../../consts";
 import { useQueryOne } from "../../enthooks/hooks/useQueryOne";
+import { AttributeMeta } from "../../ModelBoard/meta/AttributeMeta";
 import { ClassMeta, StereoType } from "../../ModelBoard/meta/ClassMeta";
 import { Meta } from "../../ModelBoard/meta/Meta";
+import { MethodMeta } from "../../ModelBoard/meta/MethodMeta";
+import { RelationMeta, RelationType } from "../../ModelBoard/meta/RelationMeta";
 import { useSelectedAppUuid } from "../../shared/AppRoot/context";
 import { classesState, entitiesState, packagesState } from "../recoil";
+import _ from "lodash";
+
+export const sort = (array: { name: string }[]) => {
+  return array.sort((a, b) => {
+    //忽略大小写
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    //name相等时
+    return 0;
+  }) as any
+}
 
 export function useBuildMeta(): { error?: Error; loading?: boolean } {
   const appUuid = useSelectedAppUuid();
@@ -54,12 +74,41 @@ export function useBuildMeta(): { error?: Error; loading?: boolean } {
 
   );
 
-  const makeEntity = useCallback((cls: ClassMeta) => {
+  const getParentClasses = useCallback((classUuid: string, classMetas: ClassMeta[], relations: RelationMeta[]) => {
+    const classes: ClassMeta[] = [];
+    for (const relation of relations) {
+      if (relation.relationType === RelationType.INHERIT) {
+        if (relation.sourceId === classUuid) {
+          const parent = classMetas.find(cls => cls.uuid === relation.targetId)
+          if (parent) {
+            classes.push(parent);
+            const parentsOfParent = getParentClasses(parent.uuid, classMetas, relations);
+            classes.push(...parentsOfParent)
+          }
+        }
+      }
+    }
+    return classes;
+  }, [])
+
+
+  const makeEntity = useCallback((cls: ClassMeta, classMetas: ClassMeta[], relations: RelationMeta[]) => {
+    const parentClasses = getParentClasses(cls.uuid, classMetas, relations);
+    const parentAttributes: AttributeMeta[] = [];
+    const parentMethods: MethodMeta[] = [];
+
+    for (const parentCls of parentClasses) {
+      parentAttributes.push(...parentCls.attributes || []);
+      parentMethods.push(...parentCls.methods || []);
+    }
+
     return {
       ...cls,
+      attributes: sort(_.uniqBy([...cls.attributes || [], ...parentAttributes], "name")),
+      methods: sort(_.uniqBy([...cls.methods || [], ...parentMethods], "name")),
       associations: []
     }
-  }, []);
+  }, [getParentClasses]);
 
   useEffect(() => {
     if (data && (systemData || appUuid === SYSTEM_APP_UUID)) {
@@ -70,10 +119,23 @@ export function useBuildMeta(): { error?: Error; loading?: boolean } {
       }
       const systemPackages = systemMeta?.content?.packages?.filter(pkg => pkg.sharable) || [];
       const systemClasses = systemMeta?.content?.classes?.filter(cls => getPackage(cls.packageUuid).sharable) || []
-      const allClasses = [...systemClasses, ...meta?.content?.classes || []];
+      const allClasses: ClassMeta[] = [...systemClasses, ...meta?.content?.classes || []];
+      const allRelations: RelationMeta[] = [...systemMeta?.content?.relations, ...meta?.content?.relations];
       setPackages([...systemPackages, ...meta?.content?.packages || []]);
       setClasses(allClasses);
-      setEntitiesState(allClasses.filter(cls => cls.stereoType === StereoType.Entity).map(cls => makeEntity(cls)) || [])
+      setEntitiesState(
+        sort(
+          allClasses.filter(
+            cls => cls.stereoType === StereoType.Entity
+          ).map(
+            cls => makeEntity(
+              cls,
+              allClasses,
+              allRelations
+            )
+          ) || []
+        )
+      )
     }
   }, [data, queryName, setClasses, setPackages, systemData, appUuid, setEntitiesState, makeEntity]);
 
