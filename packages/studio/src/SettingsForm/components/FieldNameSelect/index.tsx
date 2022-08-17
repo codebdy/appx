a10298c14f9b79a0c7ec3baab06e17f47c9c9885
Water.Li <lizongbin@hotmail.com>
2022-08-17 15:15:54 +0800

    Update index.tsx

--- packages/studio/src/SettingsForm/components/FieldNameSelect/index.tsx
+++ packages/studio/src/SettingsForm/components/FieldNameSelect/index.tsx
@@ -3,57 +3,52 @@ import React from "react";
 import "./style.less"
 import { AutoComplete, Input } from 'antd';
 
-const renderTitle = (title: string) => (
-  <span>
-    {title}
-    <a
-      style={{ float: 'right' }}
-      href="https://www.google.com/search?q=antd"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      more
-    </a>
-  </span>
-);
-
 const renderItem = (title: string, count: number) => ({
   value: title,
-  label: (
-    <div
-      style={{
-        display: 'flex',
-      }}
-    >
-      {title}
-    </div>
-  ),
+  label: title,
 });
 
 const options = [
   {
-    label: renderTitle('Libraries'),
+    label: 'Libraries',
     options: [renderItem('AntDesign', 10000), renderItem('AntDesign UI', 10600)],
   },
   {
-    label: renderTitle('Solutions'),
+    label: 'Solutions',
     options: [renderItem('AntDesign UI FAQ', 60100), renderItem('AntDesign FAQ', 30010)],
   },
   {
-    label: renderTitle('Articles'),
+    label: 'Articles',
     options: [renderItem('AntDesign design language', 100000)],
   },
 ];
 
 export const FieldNameSelect = observer(() => {
   return (
-    <AutoComplete
-      dropdownClassName="certain-category-search-dropdown"
-      dropdownMatchSelectWidth={500}
-      style={{ width: 250 }}
-      options={options}
-    >
-      <Input size="large" placeholder="input here" />
-    </AutoComplete>
+    <>
+      <AutoComplete
+        dropdownClassName="certain-category-search-dropdown"
+        //dropdownMatchSelectWidth={500}
+        options={options}
+      >
+        <Input
+          style={{
+            paddingRight: "24px"
+          }}
+          placeholder="input here"
+        />
+      </AutoComplete>
+      <span className="ant-select-arrow"
+        unselectable="on"
+        aria-hidden="true"
+        style={{ userSelect: "none", marginTop: -11 }}>
+        <span role="img" aria-label="down" className="anticon anticon-down ant-select-suffix">
+          <svg viewBox="64 64 896 896" focusable="false" data-icon="down" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            <path d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z">
+            </path>
+          </svg>
+        </span>
+      </span>
+    </>
   )
 })
