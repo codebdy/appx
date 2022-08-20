278e0b503cd7797138c17a6630b448580f340e20
Water.Li <lizongbin@hotmail.com>
2022-08-20 12:31:49 +0800

    Update index.tsx

--- packages/studio/src/components/pc/layout/ProTable/QueryForm/index.tsx
+++ packages/studio/src/components/pc/layout/ProTable/QueryForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useCallback } from 'react'
+import React, { useMemo, useState, useCallback, Children } from 'react'
 import { createForm } from '@formily/core'
 import { createSchemaField, FormProvider, observer } from '@formily/react'
 import {
@@ -14,41 +14,32 @@ import { ButtonsGridColum } from './ButtonsGridColum'
 export interface IQueryFormProps {
   maxRowsOnCollapsed?: number,
   maxColumns?: number,
+  maxWidth?: number,
   layout?: "horizontal" | "vertical",
+  collapsiable?: boolean,
   className?: string,
   children?: React.ReactNode
 }
 
 const QueryForm: React.FC = observer((props: IQueryFormProps) => {
-  const { layout = "vertical", maxRowsOnCollapsed = 1, maxColumns } = props;
+  const { layout = "vertical", maxRowsOnCollapsed = 1, maxColumns, maxWidth = 240, collapsiable } = props;
   const [expanded, setExpanded] = useState(false);
-  
+
   const grid = useMemo(
     () =>
       FormGrid.createFormGrid({
         maxColumns: maxColumns,
-        maxWidth: 240,
+        maxWidth: maxWidth,
         maxRows: maxRowsOnCollapsed,
         shouldVisible: (node, grid) => {
-          if (node.index === grid.childSize - 1) return true
-          if (grid.maxRows === Infinity) return true
-          return node.shadowRow < maxRowsOnCollapsed + 1 && node.index < maxColumns - 1
+          return true;
+          // if (node.index === grid.childSize - 1) return true
+          // if (grid.maxRows === Infinity) return true
+          // return node.shadowRow < maxRowsOnCollapsed + 1 && node.index < maxColumns - 1
         },
       }),
-    [maxColumns, maxRowsOnCollapsed]
+    [maxColumns, maxRowsOnCollapsed, maxWidth]
   )
-
-  const collapsiable = useMemo(() => {
-    const realRows = grid.shadowRows
-    const computeRows = grid.fullnessLastColumn
-      ? grid.shadowRows - 1
-      : grid.shadowRows
-
-    if (realRows < maxRowsOnCollapsed + 1) return false
-    if (computeRows > maxRowsOnCollapsed) return true
-    return false
-  }, [grid.fullnessLastColumn, grid.shadowRows, maxRowsOnCollapsed])
-
 
   const handleToggle = useCallback(() => {
     setExpanded(expanded => !expanded)
@@ -57,12 +48,41 @@ const QueryForm: React.FC = observer((props: IQueryFormProps) => {
   return (
     <FormLayout {...props} layout={layout} feedbackLayout="terse">
       <FormGrid grid={grid}>
-        {props.children}
+        <FormGrid.GridColumn>
+          <SchemaField>
+            <SchemaField.String
+              name="input2"
+              title="Input 2"
+              x-component="Input"
+              x-decorator="FormItem"
+            />
+          </SchemaField>
+        </FormGrid.GridColumn>
+        <FormGrid.GridColumn>
+          <SchemaField>
+            <SchemaField.String
+              name="input3"
+              title="Input 2"
+              x-component="Input"
+              x-decorator="FormItem"
+            />
+          </SchemaField>
+        </FormGrid.GridColumn>
+        <FormGrid.GridColumn>
+          <SchemaField>
+            <SchemaField.String
+              name="input4"
+              title="Input 2"
+              x-component="Input"
+              x-decorator="FormItem"
+            />
+          </SchemaField>
+        </FormGrid.GridColumn>
         <FormGrid.GridColumn
-          gridSpan={expanded ? -1 : 1}
+          gridSpan={-1}
           style={{
             display: 'flex',
-            justifyContent: 'right',
+            justifyContent: 'space-between',
             alignItems: layout === "horizontal" ? "flex-start" : "center",
           }}
         >
