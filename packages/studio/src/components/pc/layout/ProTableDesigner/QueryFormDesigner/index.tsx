c87b10277b56c9c5d79f1d00c681a53ba199e00b
Water.Li <lizongbin@hotmail.com>
2022-08-20 15:57:00 +0800

    Update index.tsx

--- packages/studio/src/components/pc/layout/ProTableDesigner/QueryFormDesigner/index.tsx
+++ packages/studio/src/components/pc/layout/ProTableDesigner/QueryFormDesigner/index.tsx
@@ -1,7 +1,19 @@
-import React from "react"
+import React, { memo } from "react"
 import './locales'
 import './schema'
 import { DnFC } from '@designable/react'
 import { QueryForm } from "../../ProTable/QueryForm"
+import { DroppableWidget } from "@designable/react"
 
-export const QueryFormDesigner: DnFC<React.ComponentProps<typeof QueryForm>> = QueryForm
+export const QueryFormDesigner: DnFC<React.ComponentProps<typeof QueryForm>> = memo((
+  props
+) => {
+  const { children, ...rest } = props;
+  return (
+    <QueryForm {...rest}>
+      {
+        children ? children : <DroppableWidget />
+      }
+    </QueryForm>
+  )
+}) 
