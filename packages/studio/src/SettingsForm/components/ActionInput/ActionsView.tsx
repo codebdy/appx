23b62114a597d2340ba86cd48aaea124a557f0ab
Water.Li <lizongbin@hotmail.com>
2022-08-22 10:21:34 +0800

    Update ActionsView.tsx

--- packages/studio/src/SettingsForm/components/ActionInput/ActionsView.tsx
+++ packages/studio/src/SettingsForm/components/ActionInput/ActionsView.tsx
@@ -1,10 +1,27 @@
 import React from "react"
 import { memo } from "react"
+import { Droppable } from "react-beautiful-dnd"
 
-export const ActionsView = memo(()=>{
+export const ACTIONS_VIEW_ID = "ACTIONS_VIEW_ID"
+
+export const ActionsView = memo(() => {
   return (
-    <div className="actions-view">
-      view
-    </div>
+    <Droppable droppableId={ACTIONS_VIEW_ID} >
+      {(provided, snapshot) => (
+        <div className="actions-view"
+          ref={provided.innerRef}
+          style={{
+            flex: 1,
+            flexFlow: "column",
+            backgroundColor: snapshot.isDraggingOver
+              ? "rgba(0,0,0, 0.05)"
+              : undefined,
+          }}
+        >
+
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   )
 })
