83129cf5a4c28af28a36ad419117361b0e056865
Water.Li <lizongbin@hotmail.com>
2022-08-23 09:16:13 +0800

    Update useCurrentEntity.ts

--- packages/studio/src/datasource/hooks/useCurrentEntity.ts
+++ packages/studio/src/datasource/hooks/useCurrentEntity.ts
@@ -7,18 +7,22 @@ import { useGetEntity } from './useGetEntity';
 export function useCurrentEntity() {
   const currentNode = useCurrentNode();
   const getEntity = useGetEntity();
-  const getRecentDataSource = useCallback((node?: TreeNode) => {
+  const getRecentDataSourceUuid = useCallback((node?: TreeNode) => {
+    const fieldSource = node?.parent?.props?.["x-field-source"];
+    if (fieldSource?.["typeUuid"]) {
+      return fieldSource?.["typeUuid"];
+    }
     const dataSource = node?.parent?.props?.["x-component-props"]?.["dataBindSource"]
     if (dataSource) {
       return dataSource
     } else if (node?.parent) {
-      return getRecentDataSource(node?.parent)
+      return getRecentDataSourceUuid(node?.parent)
     }
   }, [])
 
   const entity = useMemo(() => {
-    return getEntity(getRecentDataSource(currentNode)?.entityUuid);
-  }, [currentNode, getEntity, getRecentDataSource])
+    return getEntity(getRecentDataSourceUuid(currentNode));
+  }, [currentNode, getEntity, getRecentDataSourceUuid])
 
   return entity;
 }
