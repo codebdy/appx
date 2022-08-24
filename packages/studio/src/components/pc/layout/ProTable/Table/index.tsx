6b724e5f6b96e5307e237ebeb75e68dda0e3620f
Water.Li <lizongbin@hotmail.com>
2022-08-24 09:44:49 +0800

    backup

--- packages/studio/src/components/pc/layout/ProTable/Table/index.tsx
+++ packages/studio/src/components/pc/layout/ProTable/Table/index.tsx
@@ -1,6 +1,18 @@
 import { Button, Table, TableProps, Tag } from 'antd';
 import React, { memo, useMemo } from 'react';
 import { useProTableParams } from '../context';
+import { ArrayBase } from "@formily/antd"
+import {
+  useField,
+  useFieldSchema,
+  RecursionField,
+} from '@formily/react'
+
+import { GeneralField, FieldDisplayTypes, ArrayField } from '@formily/core'
+import { ColumnProps } from "antd/lib/table"
+import { Schema } from '@formily/json-schema'
+import { usePrefixCls } from "@formily/antd/esm/__builtins__"
+import { isArr } from '@formily/shared'
 
 const columns = [
   {
@@ -101,8 +113,93 @@ const onChange = (pagination, filters, sorter, extra) => {
   console.log('params', pagination, filters, sorter, extra);
 };
 
-// rowSelection object indicates the need for row selection
+interface ObservableColumnSource {
+  field: GeneralField
+  columnProps: ColumnProps<any>
+  schema: Schema
+  display: FieldDisplayTypes
+  name: string
+}
 
+const isColumnComponent = (schema: Schema) => {
+  return schema['x-component']?.indexOf('Column') > -1
+}
+
+
+// rowSelection object indicates the need for row selection
+const useArrayTableColumns = (
+  dataSource: any[],
+  sources: ObservableColumnSource[]
+): TableProps<any>['columns'] => {
+  return sources?.reduce((buf, { name, columnProps, schema, display }, key) => {
+    if (display !== 'visible') return buf
+    if (!isColumnComponent(schema)) return buf
+    return buf.concat({
+      ...columnProps,
+      key,
+      dataIndex: name,
+      render: (value: any, record: any) => {
+        const index = dataSource.indexOf(record)
+        const children = (
+          <ArrayBase.Item index={index} record={() => dataSource[index]}>
+            <RecursionField schema={schema} name={index} onlyRenderProperties />
+          </ArrayBase.Item>
+        )
+        return children
+      },
+    })
+  }, [])
+}
+
+const useArrayTableSources = () => {
+  const arrayField = useField()
+  const schema = useFieldSchema()
+  const parseSources = (schema: Schema): ObservableColumnSource[] => {
+    if (
+      isColumnComponent(schema) ||
+      isOperationsComponent(schema) ||
+      isAdditionComponent(schema)
+    ) {
+      if (!schema['x-component-props']?.['dataIndex'] && !schema['name'])
+        return []
+      const name = schema['x-component-props']?.['dataIndex'] || schema['name']
+      const field = arrayField.query(arrayField.address.concat(name)).take()
+      const columnProps =
+        field?.component?.[1] || schema['x-component-props'] || {}
+      const display = field?.display || schema['x-display']
+      return [
+        {
+          name,
+          display,
+          field,
+          schema,
+          columnProps,
+        },
+      ]
+    } else if (schema.properties) {
+      return schema.reduceProperties((buf, schema) => {
+        return buf.concat(parseSources(schema))
+      }, [])
+    }
+  }
+
+  const parseArrayItems = (schema: Schema['items']) => {
+    if (!schema) return []
+    const sources: ObservableColumnSource[] = []
+    const items = isArr(schema) ? schema : [schema]
+    return items.reduce((columns, schema) => {
+      const item = parseSources(schema)
+      if (item) {
+        return columns.concat(item)
+      }
+      return columns
+    }, sources)
+  }
+
+  if (!schema) throw new Error('can not found schema object')
+
+  return parseArrayItems(schema.items)
+}
 export const DataTable = memo((
   props: TableProps<any>
 ) => {
