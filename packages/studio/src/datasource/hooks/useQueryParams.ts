db39e25f945834bb882798a2b1c77024fa9b6a5b
Water.Li <lizongbin@hotmail.com>
2022-08-25 18:06:03 +0800

    backup

--- packages/studio/src/datasource/hooks/useQueryParams.ts
+++ packages/studio/src/datasource/hooks/useQueryParams.ts
@@ -1,15 +1,36 @@
 import { useMemo } from "react";
 import { Schema } from '@formily/json-schema';
 import { IDataBindSource } from "../model";
+import { parse, OperationTypeNode } from "graphql";
+import { message } from "antd";
+import { useTranslation } from "react-i18next";
 
 export interface IQueryParams {
   gql?: string,
   variables?: any,
-  dataField?: string,
+  rootFieldName?: string,
 }
 
 export function useQueryParams(dataBindSource?: IDataBindSource, schema?: Schema): IQueryParams {
-  const params = useMemo(() => ({}), []);
+  const params: IQueryParams = useMemo(() => ({}), []);
+  const { t } = useTranslation();
+  const firstOperationDefinition = (ast) => ast.definitions?.[0];
+  const firstFieldValueNameFromOperation = (operationDefinition) => operationDefinition?.selectionSet?.selections?.[0]?.name?.value;
+
+  if (dataBindSource.expression) {
+    try {
+      const paresedExpression = parse(dataBindSource.expression);
+      console.log("danzm", paresedExpression);
+      const operation = firstOperationDefinition(paresedExpression).operation;
+      if (operation !== OperationTypeNode.QUERY) {
+        message.error("Can not find query operation");
+      }
+      params.rootFieldName = firstFieldValueNameFromOperation(firstOperationDefinition(paresedExpression));
+    } catch (err) {
+      console.error(err);
+      message.error(t("Query.GraphqlExpressionError") + err?.message)
+    }
+  }
 
   return params
 }
