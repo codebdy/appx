72b651699b8a6f98f9ee09bc5a182da2103575a1
Water.Li <lizongbin@hotmail.com>
2022-08-25 19:26:42 +0800

    Update useQueryFragmentFromSchema.ts

--- packages/studio/src/datasource/hooks/useQueryFragmentFromSchema.ts
+++ packages/studio/src/datasource/hooks/useQueryFragmentFromSchema.ts
@@ -1,6 +1,23 @@
 import { Schema } from '@formily/json-schema';
+import { useCallback, useMemo } from 'react';
 import { IFragmentParams } from './IFragmentParams';
 
 export function useQueryFragmentFromSchema(schema?: Schema): IFragmentParams {
+  const getFragmentFromSchema = useCallback((schema) => {
+    console.log("ccc mmm", schema?.["x-field-source"]);
+    if (schema?.properties) {
+      for (const key of Object.keys(schema?.properties)) {
+        getFragmentFromSchema(schema.properties[key])
+      }
+    }
+    if (schema?.items?.properties) {
+      for (const key of Object.keys(schema?.items?.properties)) {
+        getFragmentFromSchema(schema.items.properties[key])
+      }
+    }
+
+  }, [])
+
+  const fragment = useMemo(() => getFragmentFromSchema(schema), [getFragmentFromSchema, schema])
   return {}
 }
