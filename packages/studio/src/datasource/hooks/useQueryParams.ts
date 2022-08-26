6d512a4ca5d2a88f62a57cc5ac4ab4ff890040db
Water.Li <lizongbin@hotmail.com>
2022-08-26 10:21:36 +0800

    Update useQueryParams.ts

--- packages/studio/src/datasource/hooks/useQueryParams.ts
+++ packages/studio/src/datasource/hooks/useQueryParams.ts
@@ -12,54 +12,57 @@ export interface IQueryParams extends IFragmentParams {
 }
 
 export function useQueryParams(dataBindSource?: IDataBindSource, schema?: Schema): IQueryParams {
-  const params: IQueryParams = useMemo(() => ({}), []);
   const { t } = useTranslation();
   const firstOperationDefinition = (ast) => ast.definitions?.[0];
   const firstFieldValueNameFromOperation = (operationDefinition) => operationDefinition?.selectionSet?.selections?.[0]?.name?.value;
   const fragmentFromSchema = useQueryFragmentFromSchema(schema);
-  if (dataBindSource.expression) {
-    try {
-      const ast = parse(dataBindSource.expression);
-      console.log("danzm", ast);
-      const operation = firstOperationDefinition(ast).operation;
-      if (operation !== OperationTypeNode.QUERY) {
-        message.error("Can not find query operation");
-      }
-      params.rootFieldName = firstFieldValueNameFromOperation(firstOperationDefinition(ast));
-
-      const fragmenAst = visit(firstOperationDefinition(ast)?.selectionSet?.selections?.[0]?.selectionSet, {
-        enter(node, key, parent, path, ancestors) {
-          // do some work
-        },
-        leave(node, key, parent, path, ancestors) {
-          // do some more work
+  const params = useMemo(() => {
+    const parms:IQueryParams= {}
+    if (dataBindSource.expression) {
+      try {
+        const ast = parse(dataBindSource.expression);
+        console.log("gql ast", ast);
+        const operation = firstOperationDefinition(ast).operation;
+        if (operation !== OperationTypeNode.QUERY) {
+          message.error("Can not find query operation");
         }
-      });
-
-      const rootFragment: IFragmentParams = {
-        gql: `fragment RootFragment on ${params.rootFieldName} ${print(fragmenAst)}`,
-        variables: dataBindSource.variables,
-      }
-
-      params.variables = { ...fragmentFromSchema.variables || {}, ...rootFragment.variables || {} }
-
-      const gql = `
-      fragment SchemaFragment on ${params.rootFieldName} ${rootFragment.gql}
-      ${fragmentFromSchema.gql}
-      query{
-        ${params.rootFieldName}{
-          ...RootFragment
-          ...SchemaFragment
+        parms.rootFieldName = firstFieldValueNameFromOperation(firstOperationDefinition(ast));
+  
+        const fragmenAst = visit(firstOperationDefinition(ast)?.selectionSet?.selections?.[0]?.selectionSet, {
+          enter(node, key, parent, path, ancestors) {
+            // do some work
+          },
+          leave(node, key, parent, path, ancestors) {
+            // do some more work
+          }
+        });
+  
+        const rootFragment: IFragmentParams = {
+          gql: `fragment RootFragment on ${parms.rootFieldName} ${print(fragmenAst)}`,
+          variables: dataBindSource.variables,
         }
+  
+        parms.variables = { ...fragmentFromSchema.variables || {}, ...rootFragment.variables || {} }
+  
+        const gql = `
+        fragment SchemaFragment on ${parms.rootFieldName} ${rootFragment.gql}
+        ${fragmentFromSchema.gql}
+        query{
+          ${parms.rootFieldName}{
+            ...RootFragment
+            ...SchemaFragment
+          }
+        }
+        `;
+        parms.gql = gql;
+      } catch (err) {
+        console.error(err);
+        message.error(t("Query.GraphqlExpressionError") + err?.message)
       }
-      `;
-      console.log("呵呵呵呵", gql)
-    } catch (err) {
-      console.error(err);
-      message.error(t("Query.GraphqlExpressionError") + err?.message)
     }
-  }
-
+  
+    return parms;
+  }, [dataBindSource.expression, dataBindSource.variables, fragmentFromSchema.gql, fragmentFromSchema.variables, t]);
 
   return params
 }
