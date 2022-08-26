3d7cf4b6c4116b0ac8f3bf6480f17faac877953b
Water.Li <lizongbin@hotmail.com>
2022-08-26 11:16:29 +0800

    backup

--- packages/studio/src/AppRunner/RunnerRoot.tsx
+++ packages/studio/src/AppRunner/RunnerRoot.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { SERVER_URL, SYSTEM_APP_UUID } from "../consts";
+import { EntiRoot } from "../enthooks";
 import { IMenuItem } from "../model/IMenuNode";
 import { RouteContext } from "./context/route";
 
@@ -8,10 +11,13 @@ const RunnerRoot = (
   }
 ) => {
   const [mentItem, setMenuItem] = useState<IMenuItem>()
+  const { appUuid = SYSTEM_APP_UUID } = useParams();
 
   return (
     <RouteContext.Provider value={{ menuItem: mentItem, setMenuItem: setMenuItem }}>
-      {props.children}
+      <EntiRoot config={{ endpoint: SERVER_URL, appUuid: appUuid }} >
+        {props.children}
+      </EntiRoot>
     </RouteContext.Provider>
   )
 }
