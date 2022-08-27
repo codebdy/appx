c4c4f865d20712447287d1d95648d882377bc584
Water.Li <lizongbin@hotmail.com>
2022-08-27 19:19:06 +0800

    Update preview.tsx

--- packages/studio/src/components/pc/FormButtonGroupDesigner/preview.tsx
+++ packages/studio/src/components/pc/FormButtonGroupDesigner/preview.tsx
@@ -1,8 +1,26 @@
 import { observer } from "@formily/reactive-react";
-import React from "react";
+import React, { useCallback } from "react";
+import { DnFC } from '@designable/react'
+import { FormButtonGroup } from "@formily/antd";
 
-export const FormButtonGroupDesigner = observer(()=>{
+export type IFormButtonGroupProps = React.ComponentProps<typeof FormButtonGroup> & {
+  formItem?: boolean;
+  sticky?: boolean;
+}
+
+export const FormButtonGroupDesigner: DnFC<IFormButtonGroupProps> = observer((props) => {
+  const { formItem, sticky, ...other } = props;
+  const render = useCallback(() => {
+    const group = formItem ? <FormButtonGroup.FormItem {...other} /> : <FormButtonGroup {...other} />;
+    if (sticky) {
+      return <FormButtonGroup.Sticky>
+        {group}
+      </FormButtonGroup.Sticky>
+    }
+    return group
+  }, [formItem, other, sticky])
+
   return (
-    <></>
+    render()
   )
 })
