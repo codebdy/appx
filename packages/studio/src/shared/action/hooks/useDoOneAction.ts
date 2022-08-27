dc4df9870d47a08b1d31bc3805b0c5b637e8456f
Water.Li <lizongbin@hotmail.com>
2022-08-27 11:18:17 +0800

    backup

--- packages/studio/src/shared/action/hooks/useDoOneAction.ts
+++ packages/studio/src/shared/action/hooks/useDoOneAction.ts
@@ -1,20 +1,25 @@
 import { useCallback } from "react";
 import { ActionType, IAppxAction, IOpenPageAction } from "../model";
+import { useClosePage } from "./useClosePage";
 import { useOpenPage } from "./useOpenPage";
 
 export function useDoOneAction() {
   const openPage = useOpenPage();
+  const closePage = useClosePage();
 
   const doAction = useCallback((action: IAppxAction) => {
+    
     return new Promise((resolve, reject) => {
       if (action.actionType === ActionType.OpenPage) {
         const payload = action.payload as IOpenPageAction;
         openPage(payload);
         resolve(undefined);
+      }else if(action.actionType === ActionType.ClosePage){
+        closePage();
       }
 
     })
-  }, [openPage])
+  }, [closePage, openPage])
 
   return doAction;
 }
