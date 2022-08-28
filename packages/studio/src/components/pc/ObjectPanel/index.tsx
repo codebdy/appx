d2190113ea1a19696a59ec16f2b9ac03a37c917c
Water.Li <lizongbin@hotmail.com>
2022-08-28 10:45:56 +0800

    Update index.tsx

--- packages/studio/src/components/pc/ObjectPanel/index.tsx
+++ packages/studio/src/components/pc/ObjectPanel/index.tsx
@@ -1,14 +1,17 @@
 import { observer } from "@formily/reactive-react"
 import { IDataBindSource } from "../../../datasource"
-import React from "react"
+import React, { useEffect } from "react"
 import { isObj } from "@formily/shared";
 import {
   useFieldSchema,
+  useField
 } from '@formily/react'
 import { useQueryParams } from '../../../datasource/hooks/useQueryParams';
 import { useDataQuery } from '../../../datasource/hooks/useDataQuery';
 import { useShowError } from '../../../hooks/useShowError';
 import { Spin } from "antd";
+import { Field } from '@formily/core'
+import { InstanceContext } from "../../../shared/contexts/instance";
 
 export const ObjectPanel = observer((props: {
   dataBindSource: IDataBindSource,
@@ -17,14 +20,17 @@ export const ObjectPanel = observer((props: {
   const { dataBindSource, children } = props;
   const schema = useFieldSchema();
   const queryParams = useQueryParams(dataBindSource, schema);
-
+  const field = useField();
   const { data, loading, error } = useDataQuery(queryParams);
   useShowError(error);
 
-  console.log("data addd", data)
   if (data && !isObj(data)) {
     throw new Error("Data is not object, please check query expression")
   }
+
+  useEffect(() => {
+    (field as Field).setInitialValue(data);
+  }, [data, field])
 
   return (
     loading
@@ -34,7 +40,15 @@ export const ObjectPanel = observer((props: {
       </Spin>
       :
       <>
-        {children}
+        <InstanceContext.Provider
+          value={{
+            fieldPath: field?.path?.toString(),
+            instance: data,
+            entityName: dataBindSource.entityName,
+          }}
+        >
+          {children}
+        </InstanceContext.Provider>
       </>
   )
 })
