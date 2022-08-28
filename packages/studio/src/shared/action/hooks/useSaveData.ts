b71049587e6de24357297a9e353f4d2e0648b191
Water.Li <lizongbin@hotmail.com>
2022-08-28 16:26:52 +0800

    Update useSaveData.ts

--- packages/studio/src/shared/action/hooks/useSaveData.ts
+++ packages/studio/src/shared/action/hooks/useSaveData.ts
@@ -1,19 +1,37 @@
+import { usePostOne } from "../../../enthooks/hooks/usePostOne";
 import { useCallback, useRef } from "react";
 import { useInstanceParams } from "../../contexts/instance";
+import { useShowError } from "../../../hooks/useShowError";
 
-export function useSaveData(){
+export function useSaveData() {
   const { field, entityName, instance } = useInstanceParams()
   const resolveRef = useRef<(value: unknown) => void>();
   const rejectRef = useRef<(reason?: any) => void>();
-  
-  const save = useCallback(()=>{
+  const [post, { error }] = usePostOne(entityName, {
+    onCompleted: (data: any) => {
+      resolveRef.current && resolveRef.current(data)
+    },
+    onError: (err: Error) => {
+      rejectRef.current && rejectRef.current(err)
+    }
+  })
+
+  useShowError(error);
+
+  const save = useCallback(() => {
     const p = new Promise((resolve, reject) => {
       resolveRef.current = resolve;
       rejectRef.current = reject;
       field.validate()
+        .then(() => {
+          post({ ...instance || {}, ...field.value || {} });
+        })
+        .catch((err: Error) => {
+          reject(err)
+        })
     });
     return p;
-  }, [field])
+  }, [field, instance, post])
 
   return save;
 }
