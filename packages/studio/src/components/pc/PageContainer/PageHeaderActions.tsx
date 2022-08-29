d44a6dd681d3367a7538d83b7f3567d76ee600ac
Water.Li <lizongbin@hotmail.com>
2022-08-29 09:10:24 +0800

    Update PageHeaderActions.tsx

--- packages/studio/src/components/pc/PageContainer/PageHeaderActions.tsx
+++ packages/studio/src/components/pc/PageContainer/PageHeaderActions.tsx
@@ -1,15 +1,19 @@
 import { observer } from "@formily/reactive-react"
+import { Space } from "antd"
 import React from "react"
+import clx from "classnames"
 
 export interface IHeaderActionsProps {
+  className?: string,
   children?: React.ReactNode
 }
 
 const PageHeaderActions = observer((props: IHeaderActionsProps) => {
+  const { className, ...other } = props;
   return (
-    <div className="rx-page-header-actions">
+    <Space className={clx("rx-page-header-actions", className)} {...other}>
       {props.children}
-    </div>
+    </Space>
   )
 })
 
