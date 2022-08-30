928c9a53da0f2f2439a06548d1036b8a9d2572e7
Water.Li <lizongbin@hotmail.com>
2022-08-30 12:45:12 +0800

    Update index.tsx

--- packages/studio/src/components/pc/Select/index.tsx
+++ packages/studio/src/components/pc/Select/index.tsx
@@ -1,22 +1,50 @@
 import { observer } from "@formily/reactive-react"
-import React from "react"
+import React, { useCallback, useMemo } from "react"
 import { IDataSourceableProps } from "../../common/IDataSourceableProps"
 import { Select as FormilySelect } from "@formily/antd";
 import { useDataQuery } from "../../../datasource";
 import { useFieldSchema } from '@formily/react'
 import { useQueryParams } from "../../../datasource/hooks/useQueryParams";
 import { useShowError } from "../../../hooks/useShowError";
+import { isArr } from "@formily/shared";
 
 export const Select = observer((props: IDataSourceableProps & {
   labelField?: string,
   valueField?: string,
+  value?: any;
+  onChange?: (value?: any) => void,
 }) => {
-  const { dataBind, labelField, valueField, ...other } = props;
+  const { dataBind, value, onChange, labelField, valueField, ...other } = props;
   const schema = useFieldSchema();
   const queryParams = useQueryParams(dataBind, schema);
 
   const { data, loading, error } = useDataQuery(queryParams?.gql ? queryParams : undefined);
   useShowError(error);
+
+  const toArrayValue = useCallback((value) => {
+    if (!value) {
+      return [];
+    }
+    if (!isArr(value)) {
+      return [value]
+    }
+  }, []);
+
+  const decodeValue = useCallback((value: any[]) => {
+    return value?.map(child => ({ [valueField]: child }))
+  }, [valueField])
+
+  const encodeValue = useCallback((value: { [key: string]: any }[]) => {
+    return value?.map(child => (child?.[valueField]))
+  }, [valueField])
+
+  const eodededValue = useMemo(() => {
+    return encodeValue(toArrayValue(value))
+  }, [encodeValue, toArrayValue, value])
+
+  const handleChange = useCallback((value) => {
+    onChange(decodeValue(toArrayValue(value)))
+  }, [decodeValue, onChange, toArrayValue])
 
   return (
     <FormilySelect
@@ -27,6 +55,8 @@ export const Select = observer((props: IDataSourceableProps & {
       }}
       loading={loading}
       options={data}
+      value={eodededValue}
+      onChange={handleChange}
     />
   )
 })
