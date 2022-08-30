84ed50f81d3e69d6999d04e6651c58a868ab8e2a
Water.Li <lizongbin@hotmail.com>
2022-08-30 11:01:54 +0800

    Update index.tsx

--- packages/studio/src/components/pc/Select/index.tsx
+++ packages/studio/src/components/pc/Select/index.tsx
@@ -1,15 +1,20 @@
 import { observer } from "@formily/reactive-react"
 import React from "react"
 import { IDataSourceableProps } from "../../common/IDataSourceableProps"
-import {Select as FormilySelect} from "@formily/antd";
+import { Select as FormilySelect } from "@formily/antd";
 
-export interface ISelectProps extends IDataSourceableProps {
-
-}
-
-export const Select = observer((props: ISelectProps) => {
-  const { dataBind, ...other } = props;
+export const Select = observer((props: IDataSourceableProps & {
+  labelField?: string,
+  valueField?: string,
+}) => {
+  const { dataBind, labelField = "name", valueField = "id", ...other } = props;
   return (
-    <FormilySelect {...other} />
+    <FormilySelect
+      {...other}
+      fieldNames={{
+        label: labelField,
+        value: valueField
+      }}
+    />
   )
 })
