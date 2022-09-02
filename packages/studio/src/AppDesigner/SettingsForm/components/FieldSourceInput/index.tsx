d753fbe165c63db2e7e6b1e5786a0eb5ff5d93d1
Water.Li <lizongbin@hotmail.com>
2022-09-02 16:09:30 +0800

    Update index.tsx

--- packages/studio/src/AppDesigner/SettingsForm/components/FieldSourceInput/index.tsx
+++ packages/studio/src/AppDesigner/SettingsForm/components/FieldSourceInput/index.tsx
@@ -1,5 +1,5 @@
 import { observer } from "@formily/reactive-react";
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import "./style.less"
 import { Select } from 'antd';
 import { useCurrentEntity } from "../../../../datasource/hooks/useCurrentEntity";
@@ -7,35 +7,37 @@ import { TextWidget } from '@designable/react'
 import { useParseLangMessage } from "../../../../hooks/useParseLangMessage";
 import { FieldSourceType, IFieldSource } from "../../../../datasource/model/IFieldSource";
 import { useGetEntity } from "../../../../datasource/hooks/useGetEntity";
+import { isArr } from "@formily/shared";
+import { AttributeMeta } from "../../../../ModelBoard/meta/AttributeMeta";
 const { Option, OptGroup } = Select;
 
 export const FieldSourceInput = observer((
   props: {
     mode?: 'multiple' | 'tags',
-    value?: IFieldSource,
-    onChange?: (value?: IFieldSource) => void
+    value?: IFieldSource | IFieldSource[],
+    onChange?: (value?: IFieldSource | IFieldSource[]) => void
   }
 ) => {
-  const { value, onChange, ...other } = props;
+  const { value, mode, onChange, ...other } = props;
   const currentEntity = useCurrentEntity();
   const getEntity = useGetEntity();
   const p = useParseLangMessage();
 
-  const handleChange = useCallback((value) => {
-    if (!onChange) {
-      return;
-    }
+  const createAttrSource = useCallback((attr: AttributeMeta) => {
+    return {
+      name: attr.name,
+      sourceType: FieldSourceType.Attribute,
+      label: attr.label,
+      typeUuid: attr.typeUuid,
+      typeEntityName: getEntity(attr.typeUuid)?.name,
+      dataType: attr.type as any,
+    };
+  }, [getEntity])
 
+  const handleSingleChange = useCallback((value) => {
     const attr = currentEntity?.attributes?.find(attr => attr.name === value);
     if (attr) {
-      onChange({
-        name: attr.name,
-        sourceType: FieldSourceType.Attribute,
-        label: attr.label,
-        typeUuid: attr.typeUuid,
-        typeEntityName: getEntity(attr.typeUuid)?.name,
-        dataType: attr.type as any,
-      })
+      onChange(createAttrSource(attr))
       return;
     }
     const method = currentEntity?.methods?.find(method => method.name === value);
@@ -63,10 +65,48 @@ export const FieldSourceInput = observer((
     }
 
     onChange(undefined);
-  }, [currentEntity?.associations, currentEntity?.attributes, currentEntity?.methods, getEntity, onChange])
+  }, [createAttrSource, currentEntity?.associations, currentEntity?.attributes, currentEntity?.methods, getEntity, onChange])
+
+  const handleMultiChange = useCallback((value) => {
+    if (!value) {
+      onChange(undefined);
+      return;
+    }
+
+    if (isArr(value)) {
+      onChange(value?.map(child => {
+        const attr = currentEntity?.attributes?.find(attr => attr.name === child);
+        return attr && createAttrSource(attr);
+      }))
+    }
+    else {
+      const attr = currentEntity?.attributes?.find(attr => attr.name === value);
+      onChange([createAttrSource(attr)])
+    }
+  }, [createAttrSource, currentEntity?.attributes, onChange])
+
+  const handleChange = useCallback((value) => {
+    if (!onChange) {
+      return;
+    }
+
+    if (mode === "multiple") {
+      handleMultiChange(value);
+    } else {
+      handleSingleChange(value);
+    }
+  }, [handleMultiChange, handleSingleChange, mode, onChange])
+
+  const realValue = useMemo(() => {
+    if (mode === "multiple") {
+      return (value as IFieldSource[])?.map(child => child.name);
+    } else {
+      return (value as IFieldSource)?.name;
+    }
+  }, [mode, value])
 
   return (
-    <Select allowClear value={value?.name} onChange={handleChange} {...other}>
+    <Select allowClear value={realValue} onChange={handleChange} mode={mode} {...other}>
       {
         !!currentEntity?.attributes?.length &&
         <OptGroup label={<TextWidget token="SettingComponents.FieldNameSelect.Attributes" />}>
@@ -78,7 +118,7 @@ export const FieldSourceInput = observer((
         </OptGroup>
       }
       {
-        !!currentEntity?.methods?.length &&
+        !!currentEntity?.methods?.length && mode !== "multiple" &&
         <OptGroup label={<TextWidget token="SettingComponents.FieldNameSelect.Methods" />}>
           {
             currentEntity.methods?.map(method => {
@@ -88,7 +128,7 @@ export const FieldSourceInput = observer((
         </OptGroup>
       }
       {
-        !!currentEntity?.associations?.length &&
+        !!currentEntity?.associations?.length && mode !== "multiple" &&
         <OptGroup label={<TextWidget token="SettingComponents.FieldNameSelect.Associations" />}>
           {
             currentEntity.associations?.map(assoc => {
