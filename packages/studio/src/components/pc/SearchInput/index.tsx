aedb487b6efb1736e3ad2160c54f9a5434278506
Water.Li <lizongbin@hotmail.com>
2022-09-02 16:31:20 +0800

    Update index.tsx

--- packages/studio/src/components/pc/SearchInput/index.tsx
+++ packages/studio/src/components/pc/SearchInput/index.tsx
@@ -1,14 +1,44 @@
 import { observer } from "@formily/reactive-react"
 import { Input } from "antd"
-import React from "react"
+import React, { useCallback, useMemo } from "react"
+import { useFieldSchema } from "@formily/react"
+import { IFieldSource } from "packages/studio/src/datasource/model/IFieldSource"
+import { isArr } from "@formily/shared"
+
+export interface ISearchText {
+  isFuzzy?: boolean,
+  keyword?: string,
+  fields?: string[],
+}
 
 export interface ISearchInput {
   isFuzzy?: boolean,
+  value?: ISearchText,
+  onChange?: (value?: ISearchText) => void,
 }
 
 export const SearchInput = observer((props: ISearchInput) => {
-  const { isFuzzy, ...other } = props;
+  const { isFuzzy, value, onChange, ...other } = props;
+
+  const fieldSchema = useFieldSchema();
+
+  const fields = useMemo(() => {
+    const fieldSource = fieldSchema?.["x-field-source"];
+    return isArr(fieldSource)
+      ? (fieldSource as IFieldSource[]).map(subField => subField.name)
+      : ((fieldSource as IFieldSource).name && [(fieldSource as IFieldSource).name])
+  }, [fieldSchema])
+
+  const handleChange = useCallback((event?: React.ChangeEvent<HTMLInputElement>) => {
+
+    onChange && onChange({
+      isFuzzy,
+      keyword: event.target.value,
+      fields: fields,
+    })
+  }, [fields, isFuzzy, onChange]);
+
   return (
-    <Input {...other} />
+    <Input value={value?.keyword} onChange={handleChange}  {...other} />
   )
 })
