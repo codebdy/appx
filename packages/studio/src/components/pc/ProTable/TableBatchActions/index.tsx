b515b75f5e3599df5dbf0947c166f3d42fd223cc
Water.Li <lizongbin@hotmail.com>
2022-09-05 21:49:45 +0800

    Update index.tsx

--- packages/studio/src/components/pc/ProTable/TableBatchActions/index.tsx
+++ packages/studio/src/components/pc/ProTable/TableBatchActions/index.tsx
@@ -1,3 +1,4 @@
+import { observer } from "@formily/reactive-react";
 import React, { useCallback } from "react";
 import { useProTableParams } from "../context";
 import { BatchActionsContainer } from "./BatchActionsContainer";
@@ -6,21 +7,21 @@ export interface ITableBatchActionsProps {
   children?: React.ReactNode
 }
 
-export const TableBatchActions = (
+export const TableBatchActions = observer((
   props: ITableBatchActionsProps
 ) => {
   const { children } = props;
-  const { selectedRowKeys, onSelectedChange } = useProTableParams();
+  const params = useProTableParams();
 
   const handleClear = useCallback(() => {
-    onSelectedChange([])
-  }, [onSelectedChange])
+    params.selectedRowKeys = [];
+  }, [params])
 
   return (
-    selectedRowKeys?.length
-      ? <BatchActionsContainer counts={selectedRowKeys.length} onClear={handleClear}>
+    params?.selectedRowKeys?.length
+      ? <BatchActionsContainer counts={params?.selectedRowKeys?.length} onClear={handleClear}>
         {children}
       </BatchActionsContainer>
       : <></>
   )
-}
+})
