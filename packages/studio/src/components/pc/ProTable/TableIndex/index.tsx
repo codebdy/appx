3cc0f4dd8fed601149fda48d1acadae7402df583
Water.Li <lizongbin@hotmail.com>
2022-09-05 13:04:44 +0800

    Update index.tsx

--- packages/studio/src/components/pc/ProTable/TableIndex/index.tsx
+++ packages/studio/src/components/pc/ProTable/TableIndex/index.tsx
@@ -1,13 +1,22 @@
 import { ArrayBase } from "@formily/antd"
 import { usePrefixCls } from "@formily/antd/esm/__builtins__"
-import React from "react"
+import { observer } from "@formily/reactive-react"
+import React, { useMemo } from "react"
+import { useProTableParams } from "../context"
 
-export const TableIndex = (props)=>{
+export const TableIndex = observer((props) => {
   const index = ArrayBase.useIndex()
+  const params = useProTableParams();
   const prefixCls = usePrefixCls('formily-array-base')
+  const offset = useMemo(() => {
+    const pageNumber = params.current ? (params.current - 1) : 0;
+    const pageSize = params.pageSize || 10;
+
+    return pageNumber * pageSize
+  }, [params])
   return (
     <span {...props} className={`${prefixCls}-index`}>
-      #{index + 1}.
+      #{index + 1 + offset}.
     </span>
   )
-}
+})
