5b4ed0b72722c745e151eecb41c24bb03d26702a
Water.Li <lizongbin@hotmail.com>
2022-09-08 15:39:56 +0800

    backup

---
+++ packages/studio/src/components/pc/Dialog/index.tsx
@@ -0,0 +1,17 @@
+import { IIcon } from "../../../shared/icon/model"
+import React, { CSSProperties } from "react"
+
+export interface IDialogProps{
+  title?: string,
+  icon?: IIcon,
+  style?: CSSProperties,
+  children?: React.ReactNode,
+}
+
+export const  Dialog = ()=>{
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ packages/studio/src/components/pc/DialogDesigner/preview.tsx
@@ -0,0 +1,116 @@
+import React, { useCallback, useRef, useState } from 'react'
+import { TreeNode, createBehavior, createResource } from '@designable/core'
+import {
+  DnFC,
+  useTreeNode,
+} from '@designable/react'
+import { observer } from '@formily/reactive-react'
+import './styles.less'
+import { LoadTemplate } from '@designable/formily-antd/lib/common/LoadTemplate'
+import { createFieldSchema } from "../../common/Field";
+import { DialogSchema } from './schema'
+import { DialogLocales } from './locales'
+import { Button } from 'antd'
+import { CloseOutlined, EllipsisOutlined } from '@ant-design/icons'
+import { IconView } from '../../../shared/icon/IconView'
+import { useParseLangMessage } from '../../../hooks/useParseLangMessage'
+import { PopupButton } from '../../common/PopupButton'
+import { IDialogProps } from '../Dialog'
+
+export const DialogDesigner: DnFC<IDialogProps> = observer((props) => {
+  const { title, icon, children, ...other } = props;
+  const [visible, setVisiable] = useState(false);
+  const ref = useRef<HTMLElement>(null)
+  const node = useTreeNode()
+  const p = useParseLangMessage();
+
+  const handleToggleVisiable = useCallback(() => {
+    setVisiable(visible => !visible);
+  }, [])
+
+  return (
+    <>
+      {visible &&
+        <div className='rx-dialog'>
+          {children}
+          <LoadTemplate
+            actions={[
+              {
+                title: node.getMessage('addItem'),
+                icon: 'AddOperation',
+                onClick: () => {
+                  const item = new TreeNode({
+                    componentName: 'Field',
+                    props: {
+                      type: 'void',
+                      'x-component': 'DropdownMenu.Item',
+                      'x-component-props': {
+                        title: 'Menu Item'
+                      }
+                    },
+                  })
+                  node.append(item)
+                },
+              },
+            ]}
+          />
+          <PopupButton
+            icon={<CloseOutlined style={{ fontSize: 12 }} />}
+            onToggleVisiable={handleToggleVisiable}
+          />
+        </div>
+      }
+      <div style={{ position: "relative" }}>
+        <Button
+          icon={icon && <IconView icon={icon} />}
+          {...other}
+          ref={ref}
+        >
+          {
+            p(title)
+          }
+        </Button>
+
+        {
+          !visible &&
+          <PopupButton
+            icon={<EllipsisOutlined style={{ fontSize: 12 }} />}
+            style={{
+              top: 8,
+            }}
+            onToggleVisiable={handleToggleVisiable}
+          />
+        }
+      </div>
+    </>
+  )
+})
+
+DialogDesigner.Behavior = createBehavior(
+  {
+    name: 'Dialog',
+    extends: ['Field'],
+    selector: (node) => node.props['x-component'] === 'Dialog',
+    designerProps: {
+      droppable: true,
+      propsSchema: createFieldSchema(DialogSchema),
+    },
+    designerLocales: DialogLocales,
+  },
+)
+
+DialogDesigner.Resource = createResource({
+  icon: 'SelectSource',
+  elements: [
+    {
+      componentName: 'Field',
+      props: {
+        type: 'void',
+        'x-component': 'Dialog',
+        'x-component-props': {
+          title: "Dialog",
+        },
+      },
+    },
+  ],
+})
