ffca15028cc2948ab5e2208d817cee231e2d13aa
Water.Li <lizongbin@hotmail.com>
2022-09-09 11:51:53 +0800

    backup

--- packages/studio/src/components/pc/DialogDesigner/DialogContentDesigner/schema.ts
+++ packages/studio/src/components/pc/DialogDesigner/DialogContentDesigner/schema.ts
@@ -2,14 +2,4 @@ import { DialogSchema } from "../schema";
 
 DialogSchema.Content = {
   type: 'object',
-  properties: {
-    gridSpan: {
-      type: 'number',
-      'x-decorator': 'FormItem',
-      'x-component': 'NumberPicker',
-      'x-component-props': {
-        defaultValue: 1,
-      },
-    },
-  },
 }
--- packages/studio/src/components/pc/DialogDesigner/DialogFooterDesigner/schema.ts
+++ packages/studio/src/components/pc/DialogDesigner/DialogFooterDesigner/schema.ts
@@ -2,14 +2,4 @@ import { DialogSchema } from "../schema";
 
 DialogSchema.Footer = {
   type: 'object',
-  properties: {
-    gridSpan: {
-      type: 'number',
-      'x-decorator': 'FormItem',
-      'x-component': 'NumberPicker',
-      'x-component-props': {
-        defaultValue: 1,
-      },
-    },
-  },
 }
