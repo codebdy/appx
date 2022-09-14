03e43338f26d1256c05cbabecac437298a7c6aaf
Water.Li <lizongbin@hotmail.com>
2022-09-14 14:54:58 +0800

    backup

--- packages/studio/src/components/Medias/preview/pc/index.tsx
+++ packages/studio/src/components/Medias/preview/pc/index.tsx
@@ -1,6 +1,6 @@
 import { CloudUploadOutlined, FilterOutlined, HomeOutlined, SortAscendingOutlined } from "@ant-design/icons"
 import { observer } from "@formily/reactive-react"
-import { Breadcrumb, Button, Input, PageHeader, Space } from "antd"
+import { Breadcrumb, Button, Input, Space } from "antd"
 import { ResizableColumn } from "../../../../common/ResizableColumn"
 import React from "react"
 import "./style.less"
@@ -13,24 +13,25 @@ export interface IMediasProps {
 export const Medias = observer((props: IMediasProps) => {
   return (
     <div className="rx-medias top-border">
-      <PageHeader
-        className="media-page-header"
-        title="媒体管理"
-        subTitle={<Input.Search />}
-        extra={
+      <div className="media-page-header">
+        <div className="ant-page-header-heading-title">媒体管理</div>
+
+        <div className="medias-actions">
+          <div className="search-wrap">
+            <Input.Search />
+          </div>
           <Space>
             <Button type="text" shape="circle" icon={<FilterOutlined />} />
             <Button type="text" shape="circle" icon={<SortAscendingOutlined />} />
             <Button type="primary" shape="round" icon={<CloudUploadOutlined />}>上传</Button>
           </Space>
-        }
-      >
-      </PageHeader>
+        </div>
+      </div>
       <div className="medias-body">
         <ResizableColumn maxWidth={460} minWidth={160}>
           <div className="medias-left-tree right-border top-border">
             <div className="tree-title bottom-border">
-              <h3>目录</h3>
+              <h3>分类</h3>
             </div>
             <div className="tree-body">
               <FolderTree />
