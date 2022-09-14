7c81bb1e5097aa1a52f644bef0114b8e06dba09a
Water.Li <lizongbin@hotmail.com>
2022-09-14 13:39:04 +0800

    backup

--- packages/studio/src/components/Medias/preview/pc/index.tsx
+++ packages/studio/src/components/Medias/preview/pc/index.tsx
@@ -1,6 +1,7 @@
 import { CloudUploadOutlined, FilterOutlined, SortAscendingOutlined } from "@ant-design/icons"
 import { observer } from "@formily/reactive-react"
 import { Button, Input, PageHeader, Space } from "antd"
+import { ResizableColumn } from "../../../../common/ResizableColumn"
 import React from "react"
 import "./style.less"
 
@@ -10,11 +11,11 @@ export interface IMediasProps {
 
 export const Medias = observer((props: IMediasProps) => {
   return (
-    <div>
+    <div className="rx-medias">
       <PageHeader
         className="media-page-header"
         title="媒体管理"
-        subTitle = {<Input.Search />}
+        subTitle={<Input.Search />}
         extra={
           <Space>
             <Button type="text" shape="circle" icon={<FilterOutlined />} />
@@ -24,7 +25,14 @@ export const Medias = observer((props: IMediasProps) => {
         }
       >
       </PageHeader>
-      哈哈
+      <div className="medias-body">
+        <ResizableColumn maxWidth={360} minWidth={160}>
+          呵呵呵
+        </ResizableColumn>
+        <div >
+          哈哈哈
+        </div>
+      </div>
     </div>
   )
 })
