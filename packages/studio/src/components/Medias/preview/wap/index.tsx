3980a09252269e999c6934a1463f22e6107efa4f
Water.Li <lizongbin@hotmail.com>
2022-09-14 10:33:44 +0800

    backup

---
+++ packages/studio/src/components/Medias/preview/wap/index.tsx
@@ -0,0 +1,11 @@
+import { observer } from "@formily/reactive-react"
+import React from "react"
+import "./style.less"
+
+export const Medias = observer(()=>{
+  return (
+    <div>
+      
+    </div>
+  )
+})
