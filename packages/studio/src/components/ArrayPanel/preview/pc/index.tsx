10c8bd15bf083574aa6e94af8513a42eccb24470
Water.Li <lizongbin@hotmail.com>
2022-09-16 11:02:27 +0800

    Update index.tsx

--- packages/studio/src/components/ArrayPanel/preview/pc/index.tsx
+++ packages/studio/src/components/ArrayPanel/preview/pc/index.tsx
@@ -1,5 +1,13 @@
-import { observer } from "@formily/reactive-react"
 import React from "react"
+import {
+  useField,
+  observer,
+  useFieldSchema,
+  RecursionField,
+} from '@formily/react'
+import { ArrayField } from "@formily/core";
+import { ArrayBase } from "@formily/antd";
+import { Empty } from "antd";
 
 export interface IArrayPanelProps {
   value?: boolean,
@@ -7,12 +15,47 @@ export interface IArrayPanelProps {
 }
 
 export const ArrayPanel = observer((props: IArrayPanelProps) => {
-  const { value, onChange, ...other } = props;
+  const field = useField<ArrayField>()
+  const schema = useFieldSchema()
+  const dataSource = Array.isArray(field.value) ? field.value : []
+
+  if (!schema) throw new Error('can not found schema object')
+
+  const renderItems = () => {
+    return dataSource?.map((item, index) => {
+      const items = Array.isArray(schema.items)
+        ? schema.items[index] || schema.items[0]
+        : schema.items
+
+      const content = (
+        <RecursionField
+          schema={items}
+          name={index}
+        />
+      )
+      return (
+        <ArrayBase.Item
+          key={index}
+          index={index}
+          record={() => dataSource[index]}
+        >
+          {content}
+        </ArrayBase.Item>
+      )
+    })
+  }
+
+  const renderEmpty = () => {
+    if (dataSource?.length) return
+    return (
+      <Empty />
+    )
+  }
 
   return (
-    <div {...other}>
-      {
-      }
-    </div>
+    <ArrayBase>
+      {renderEmpty()}
+      {renderItems()}
+    </ArrayBase>
   )
 })
