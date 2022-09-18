612f245b7d6d209453b3bdeb1625f5c5aec2065f
Water.Li <lizongbin@hotmail.com>
2022-09-18 15:40:25 +0800

    backup

--- packages/studio/src/AppDesigner/widgets/MaterialWidget/MaterialDialog/TabsEditDialog.tsx
+++ packages/studio/src/AppDesigner/widgets/MaterialWidget/MaterialDialog/TabsEditDialog.tsx
@@ -1,9 +1,12 @@
 import { EditOutlined } from '@ant-design/icons';
 import { Button, Modal } from 'antd';
 import React, { memo, useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const TabsEditDialog = memo(() => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { t } = useTranslation();
 
   const handleShowModal = useCallback(() => {
     setIsModalOpen(true);
@@ -20,10 +23,20 @@ export const TabsEditDialog = memo(() => {
   return (
     <>
       <Button size='small' type="text" shape='circle' icon={<EditOutlined />} style={{ marginRight: 8 }} onClick={handleShowModal} />
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Modal
+        title={t("Materials.TabsEdit")}
+        open={isModalOpen}
+        okText={t("Confirm")}
+        cancelText={t("Cancel")}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <div  className='tabs-edit-content'>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <Button type='dashed' block>{t("Materials.Add")}</Button>
+        </div>
       </Modal>
     </>
   );
