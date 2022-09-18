cb32869fd3eec54cbf0a2e1f244e5e9229a10bf8
Water.Li <lizongbin@hotmail.com>
2022-09-18 13:22:26 +0800

    backup

--- packages/studio/src/AppDesigner/widgets/MaterialWidget/MaterialDialog/MaterialTabs.tsx
+++ packages/studio/src/AppDesigner/widgets/MaterialWidget/MaterialDialog/MaterialTabs.tsx
@@ -1,23 +1,116 @@
 import { Tabs } from 'antd';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+const type = 'DraggableTabNode';
 
-export const MaterialTabs: React.FC = () => {
-  const onChange = (key: string) => {
-    console.log(key);
+const DraggableTabNode = ({ index, children, moveNode }) => {
+  const ref = useRef(null);
+  const [{ isOver, dropClassName }, drop] = useDrop({
+    accept: type,
+    collect: (monitor) => {
+      const { index: dragIndex } = monitor.getItem() || {};
+
+      if (dragIndex === index) {
+        return {};
+      }
+
+      return {
+        isOver: monitor.isOver(),
+        dropClassName: 'dropping',
+      };
+    },
+    drop: (item) => {
+      moveNode(item.index, index);
+    },
+  });
+  const [, drag] = useDrag({
+    type,
+    item: {
+      index,
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+  drop(drag(ref));
+  return (
+    <div
+      ref={ref}
+      style={{
+        marginRight: 24,
+      }}
+      className={isOver ? dropClassName : ''}
+    >
+      {children}
+    </div>
+  );
+};
+
+const DraggableTabs = (props) => {
+  const { items = [] } = props;
+  const [order, setOrder] = useState([]);
+
+  const moveTabNode = (dragKey, hoverKey) => {
+    const newOrder = order.slice();
+    items.forEach((item) => {
+      if (item.key && newOrder.indexOf(item.key) === -1) {
+        newOrder.push(item.key);
+      }
+    });
+    const dragIndex = newOrder.indexOf(dragKey);
+    const hoverIndex = newOrder.indexOf(hoverKey);
+    newOrder.splice(dragIndex, 1);
+    newOrder.splice(hoverIndex, 0, dragKey);
+    setOrder(newOrder);
   };
 
-  
+  const renderTabBar = (tabBarProps, DefaultTabBar) => (
+    <DefaultTabBar {...tabBarProps}>
+      {(node) => (
+        <DraggableTabNode key={node.key} index={node.key} moveNode={moveTabNode}>
+          {node}
+        </DraggableTabNode>
+      )}
+    </DefaultTabBar>
+  );
+
+  const orderItems = [...items].sort((a, b) => {
+    const orderA = order.indexOf(a.key);
+    const orderB = order.indexOf(b.key);
+
+    if (orderA !== -1 && orderB !== -1) {
+      return orderA - orderB;
+    }
+
+    if (orderA !== -1) {
+      return -1;
+    }
+
+    if (orderB !== -1) {
+      return 1;
+    }
+
+    const ia = items.indexOf(a);
+    const ib = items.indexOf(b);
+    return ia - ib;
+  });
   return (
-    <Tabs defaultActiveKey="1">
-      <Tabs.TabPane tab="Tab 1" key="1">
-        Content of Tab Pane 1
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Tab 2" key="2">
-        Content of Tab Pane 2
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Tab 3" key="3">
-        Content of Tab Pane 3
-      </Tabs.TabPane>
-    </Tabs>
-  )
+    <DndProvider backend={HTML5Backend}>
+      <Tabs renderTabBar={renderTabBar} {...props} items={orderItems} />
+    </DndProvider>
+  );
 };
+
+export const MaterialTabs = () => (
+  <DraggableTabs
+    items={new Array(3).fill(null).map((_, i) => {
+      const id = String(i + 1);
+      return {
+        label: `tab ${id}`,
+        key: id,
+        children: `Content of Tab Pane ${id}`,
+      };
+    })}
+  />
+);
