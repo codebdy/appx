64eca3652350788ba8f45d018e784d2a425e4aae
Water.Li <lizongbin@hotmail.com>
2022-09-19 19:00:26 +0800

    Update index.tsx

--- packages/studio/src/AppDesigner/widgets/MaterialWidget/MaterialDialog/PluginList/index.tsx
+++ packages/studio/src/AppDesigner/widgets/MaterialWidget/MaterialDialog/PluginList/index.tsx
@@ -2,16 +2,11 @@ import { useAppParams } from "../../../../../shared/AppRoot/context";
 import React from "react";
 import { memo } from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
-import { DraggableLabel } from "../../../../common/DraggableLabel";
-import { useGetPluginLocalMessage } from "../../../../../plugin-sdk/hooks/useGetPluginLocalMessage";
-import { Collapse } from "antd";
 import { PluginPanel } from "./PluginPanel";
-const { Panel } = Collapse;
 
 export const PLUGINS_LIST_ID = "PLUGINS_LIST_ID";
 export const PluginList = memo(() => {
   const { plugins } = useAppParams();
-  const { getTitle } = useGetPluginLocalMessage();
 
   return (
     <Droppable droppableId={PLUGINS_LIST_ID} isDropDisabled={true}>
