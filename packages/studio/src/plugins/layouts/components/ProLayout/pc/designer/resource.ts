f43db0d6781e80b55e74a3dec1db41a5dc92761f
Water.Li <lizongbin@hotmail.com>
2022-09-20 19:29:06 +0800

    Update resource.ts

--- packages/studio/src/plugins/layouts/components/ProLayout/pc/designer/resource.ts
+++ packages/studio/src/plugins/layouts/components/ProLayout/pc/designer/resource.ts
@@ -5,7 +5,7 @@ export const resource = {
       componentName: 'Field',
       props: {
         type: 'void',
-        'x-component': 'Card',
+        'x-component': 'ProLayout',
         'x-component-props': {
           title: 'Title',
         },
