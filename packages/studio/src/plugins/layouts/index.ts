7d8cf67d7dcaace5f9de91a13f7c4b7ca47fcd25
Water.Li <lizongbin@hotmail.com>
2022-09-20 17:23:04 +0800

    Update index.ts

--- packages/studio/src/plugins/layouts/index.ts
+++ packages/studio/src/plugins/layouts/index.ts
@@ -4,10 +4,6 @@ import { rxPlugin } from "./Plugin";
 declare const window: Window & { rxPlugin: IPlugin };
 
 (function () {
-  // if(window.materials){
-  //   console.error("Has material not finished! load error", window.materials)
-  // }else{
   window.rxPlugin = rxPlugin
   console.log("Layouts plugin 中")
-  // }
 })()
