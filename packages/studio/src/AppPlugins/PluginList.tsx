48ae7f2e6a305002d76797a1a7beef9bbee0a945
Water.Li <lizongbin@hotmail.com>
2022-09-22 16:13:30 +0800

    Update PluginList.tsx

--- packages/studio/src/AppPlugins/PluginList.tsx
+++ packages/studio/src/AppPlugins/PluginList.tsx
@@ -1,12 +1,18 @@
 import { List } from 'antd';
 import React, { memo, useMemo } from 'react';
+import { PluginType } from '../model';
 import { useAppParams } from '../shared/AppRoot/context';
 import { PluginItem } from './PluginItem';
 
 export const PluginList = memo(() => {
   const { plugins } = useAppParams();
 
-  const items = useMemo(() => [...plugins], [plugins]);
+  const items = useMemo(() => {
+    return [
+      ...plugins?.filter(plugin => plugin.pluginInfo?.type === PluginType.normal) || [],
+      ...plugins?.filter(plugin => plugin.pluginInfo?.type === PluginType.debug) || []
+    ]
+  }, [plugins]);
 
   return (
     <List
