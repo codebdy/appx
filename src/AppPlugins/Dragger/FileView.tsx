a9c064afaefbb946186c2c1d36368d95ff85f22d
Water.Li <lizongbin@hotmail.com>
2022-09-23 14:42:18 +0800

    backup

--- src/AppPlugins/Dragger/FileView.tsx
+++ src/AppPlugins/Dragger/FileView.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { memo } from "react"
-import { formatFileSize } from "../../shared/formatFileSize";
 import cls from "classnames";
+import { DeleteOutlined, PaperClipOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 
 export enum FileStatus {
   waiting = 1,
@@ -23,8 +24,15 @@ export const FileView = memo((
   const { task, onRemove } = props;
 
   return (
-    <div className={cls("upload-file-item")}>
-      {task.file.name} - {formatFileSize(task.file.size)}
+    <div className={cls("upload-file-item")} title="xxxx">
+      <div className="item-text">
+        <PaperClipOutlined style={{ marginRight: 4 }} />
+        {task.file.name}
+      </div>
+
+      <div className="upload-file-action">
+        <Button type="text" size="small" icon={<DeleteOutlined />}></Button>
+      </div>
     </div>
   )
 })
