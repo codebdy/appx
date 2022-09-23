a5aa5b8c4463e1316cc0eaeb1c9d281aa5170fa8
Water.Li <lizongbin@hotmail.com>
2022-09-23 18:46:53 +0800

    Update useLoadPlugins.ts

--- src/plugin/hooks/useLoadPlugins.ts
+++ src/plugin/hooks/useLoadPlugins.ts
@@ -1,9 +1,14 @@
 import { useCallback } from "react";
+import { useAppParams } from "../../shared/AppRoot/context";
 import { IPluginInfo, PluginType } from "../../model";
 import { IInstalledPlugin, PluginStatus } from "../model";
+import { useGetPluginInfo } from "./useGetPluginInfo";
 import { loadDebugPlugin, loadPlugin } from "./useLoadPlugin";
 
 export function useLoadPlugins() {
+  const { app } = useAppParams();
+  
+  const getPlugInfo = useGetPluginInfo();
   const load = useCallback(async (pluginInfos: IPluginInfo[]) => {
     const installedPlugins: IInstalledPlugin[] = [];
     for (const pluginInfo of pluginInfos) {
@@ -14,13 +19,21 @@ export function useLoadPlugins() {
           : await loadPlugin(pluginInfo.url)
 
         installedPlugins.push({
-          pluginInfo,
+          pluginInfo: {
+            ...getPlugInfo(plugin, pluginInfo.url, pluginInfo.type),
+            ...pluginInfo || {},
+          },
           plugin,
           status: PluginStatus.Normal
         })
       } catch (error) {
         installedPlugins.push({
-          pluginInfo,
+          pluginInfo: {
+            ...pluginInfo || {},
+            appUuid: app?.uuid,
+            url: pluginInfo.url,
+            type: pluginInfo.type
+          },
           status: PluginStatus.Error
         })
       }
