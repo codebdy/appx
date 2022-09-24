6510faf136a6ae32687450058f21ab010b353ada
Water.Li <lizongbin@hotmail.com>
2022-09-24 11:48:17 +0800

    备份，未编译

---
+++ src/plugins/frameworks/pc/TopMiddleBottom/designer/resource.ts
@@ -0,0 +1,14 @@
+export const resource = {
+  icon: 'CardSource',
+  elements: [
+    {
+      componentName: 'Field',
+      props: {
+        type: 'void',
+        'x-component': 'ProLayout',
+        'x-component-props': {
+        },
+      },
+    },
+  ],
+}
---
+++ src/plugins/frameworks/pc/TopMiddleBottom/index.ts
@@ -0,0 +1,16 @@
+import { IMaterialComponent } from "@rxdrag/appx-plugin-sdk";
+import { NAME } from "../consts";
+import { ProLayoutDesigner } from "./designer";
+import { behaviors } from "./designer/behaviors";
+import { resource } from "./designer/resource";
+import { ProLayout } from "./view";
+
+const pc: IMaterialComponent = {
+  name: NAME,
+  designer: ProLayoutDesigner,
+  component: ProLayout,
+  behaviors,
+  resource
+}
+
+export default pc;
