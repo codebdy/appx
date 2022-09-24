8ef777f942e2a32332eeccc51352ab382870bba4
Water.Li <lizongbin@hotmail.com>
2022-09-24 12:38:01 +0800

    backup

--- src/plugins/frameworks/pc/HeaderContentFooterFrame/designer/index.tsx
+++ src/plugins/frameworks/pc/HeaderContentFooterFrame/designer/index.tsx
@@ -1,15 +1,42 @@
 import React from "react"
 import { DnFC } from '@designable/react'
 import { observer } from "@formily/reactive-react"
-import { ICompoentProps } from "../view"
+import { IComponentProps } from "../view"
+import { Breadcrumb, Layout, Menu } from "antd";
+import cls from "classnames";
+const { Header, Content, Footer } = Layout;
 
-const ComponentDesigner: DnFC<ICompoentProps> = observer((
+const ComponentDesigner: DnFC<IComponentProps> = observer((
   props
 ) => {
+  const {className, ...other} = props;
   return (
-    <div {...props}>
-      哈哈
-    </div>
+    <Layout className={cls("layout", className)}  {...other}>
+      <Header>
+        <div className="app-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   )
 })
 
