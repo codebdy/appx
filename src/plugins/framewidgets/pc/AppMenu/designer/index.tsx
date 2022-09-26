e002b31c6de47068b46806c65453a60ceb810378
Water.Li <lizongbin@hotmail.com>
2022-09-26 14:24:50 +0800

    Update index.tsx

--- src/plugins/framewidgets/pc/AppMenu/designer/index.tsx
+++ src/plugins/framewidgets/pc/AppMenu/designer/index.tsx
@@ -2,12 +2,58 @@ import React from "react"
 import { DnFC } from '@designable/react'
 import { observer } from "@formily/reactive-react"
 import Component, { IComponentProps } from "../view"
+import { Menu, MenuProps } from "antd";
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from "@ant-design/icons";
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+const items: MenuProps['items'] = [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
+    getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
+  ]),
+
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+  ]),
+
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+];
 
 const ComponentDesigner: DnFC<IComponentProps> = observer((
   props
 ) => {
   return (
-    <Component {...props} />
+    <Menu
+      {...props}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
+      items={items}
+    />
   )
 })
 
