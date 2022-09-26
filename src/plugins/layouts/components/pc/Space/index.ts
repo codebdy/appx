ab9020592e9fe779343d8ab11845cb60eb90ca2a
Water.Li <lizongbin@hotmail.com>
2022-09-26 17:46:40 +0800

    backup

--- src/plugins/layouts/components/pc/Space/index.ts
+++ src/plugins/layouts/components/pc/Space/index.ts
@@ -1,7 +1,6 @@
-import { Device, IMaterialComponent } from "@rxdrag/appx-plugin-sdk";
 import ComponentDesigner from "./designer";
 import behaviors from "./designer/behaviors";
-import resource from "./designer/resource";
+import resources from "./designer/resources";
 import Name from "./name";
 import { Space as FormilySpace } from '@formily/antd'
 
@@ -10,5 +9,5 @@ export default {
   designer: ComponentDesigner,
   component: FormilySpace,
   behaviors,
-  resource
+  resources
 }
