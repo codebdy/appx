146a8ae2e0999c7f8f9d9df2c5b22233dfe78bae
Water.Li <lizongbin@hotmail.com>
2022-10-01 20:55:09 +0800

    backup

--- src/AppUml/PropertyPanel/MethodPanel/ArgsInput/ArgsInput.tsx
+++ src/AppUml/PropertyPanel/MethodPanel/ArgsInput/ArgsInput.tsx
@@ -4,63 +4,43 @@ import React, { useCallback, useState } from "react"
 import { memo } from "react"
 import { useTranslation } from "react-i18next";
 import "./style.less";
-import { Space, Table, Tag } from 'antd';
+import { Table } from 'antd';
 import { ColumnsType } from 'antd/es/table';
+import { MenuOutlined } from '@ant-design/icons';
+import type { SortableContainerProps, SortEnd } from 'react-sortable-hoc';
+import { SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
+import { arrayMoveImmutable } from "array-move";
 
 interface DataType {
   key: string;
   name: string;
   age: number;
   address: string;
-  tags: string[];
+  index: number;
 }
+
+const DragHandle = SortableHandle(() => <MenuOutlined style={{ cursor: 'grab', color: '#999' }} />) as any;
 
 const columns: ColumnsType<DataType> = [
   {
+    title: 'Sort',
+    dataIndex: 'sort',
+    width: 30,
+    className: 'drag-visible',
+    render: () => <DragHandle />,
+  },
+  {
     title: 'Name',
     dataIndex: 'name',
-    key: 'name',
-    render: text => <a>{text}</a>,
+    className: 'drag-visible',
   },
   {
     title: 'Age',
     dataIndex: 'age',
-    key: 'age',
   },
   {
     title: 'Address',
     dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
   },
 ];
 
@@ -70,28 +50,61 @@ const data: DataType[] = [
     name: 'John Brown',
     age: 32,
     address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
+    index: 0,
   },
   {
     key: '2',
     name: 'Jim Green',
     age: 42,
     address: 'London No. 1 Lake Park',
-    tags: ['loser'],
+    index: 1,
   },
   {
     key: '3',
     name: 'Joe Black',
     age: 32,
     address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
+    index: 2,
   },
 ];
 
+const SortableItem: any = SortableElement((props: React.HTMLAttributes<HTMLTableRowElement>) => (
+  <tr {...props} />
+));
+const SortableBody: any = SortableContainer((props: React.HTMLAttributes<HTMLTableSectionElement>) => (
+  <tbody {...props} />
+));
 export const ArgsInput = memo(() => {
   const [open, setOpen] = useState(false);
-
+  const [dataSource, setDataSource] = useState(data);
   const { t } = useTranslation();
+
+  const onSortEnd = ({ oldIndex, newIndex }: SortEnd) => {
+    if (oldIndex !== newIndex) {
+      const newData = arrayMoveImmutable(dataSource.slice(), oldIndex, newIndex).filter(
+        (el: DataType) => !!el,
+      );
+      console.log('Sorted items: ', newData);
+      setDataSource(newData);
+    }
+  };
+
+  const DraggableContainer = (props: SortableContainerProps) => (
+    <SortableBody
+      useDragHandle
+      disableAutoscroll
+      helperClass="row-dragging"
+      onSortEnd={onSortEnd}
+      {...props}
+    />
+  );
+
+  const DraggableBodyRow: React.FC<any> = ({ className, style, ...restProps }) => {
+    // function findIndex base on Table rowKey props and should always be a right array index
+    const index = dataSource.findIndex(x => x.index === restProps['data-row-key']);
+    return <SortableItem index={index} {...restProps} />;
+  };
+
 
   const showModal = useCallback(() => {
     setOpen(true)
@@ -119,7 +132,18 @@ export const ArgsInput = memo(() => {
         cancelText={t("Cancel")}
       >
         <div className="args-input-body">
-          <Table columns={columns} dataSource={data} />
+          <Table
+            pagination={false}
+            dataSource={dataSource}
+            columns={columns}
+            rowKey="index"
+            components={{
+              body: {
+                wrapper: DraggableContainer,
+                row: DraggableBodyRow,
+              },
+            }}
+          />
           <Button type="dashed" block icon={<PlusOutlined />}>
             {t("Add")}
           </Button>
