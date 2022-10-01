e490135a4ee8b12f2267c6dfcb4ec84086503085
Water.Li <lizongbin@hotmail.com>
2022-10-01 19:27:33 +0800

    Update ScriptInput.tsx

--- src/AppUml/PropertyPanel/MethodPanel/ScriptInput.tsx
+++ src/AppUml/PropertyPanel/MethodPanel/ScriptInput.tsx
@@ -1,9 +1,39 @@
-import React from "react"
+import { Button, Modal } from "antd"
+import React, { useCallback, useState } from "react"
 import { memo } from "react"
+import { useTranslation } from "react-i18next";
 
 export const ScriptInput = memo(() => {
+  const [open, setOpen] = useState(false);
+
+  const { t } = useTranslation();
+
+  const showModal = useCallback(() => {
+    setOpen(true)
+  }, [])
+
+  const handleOk = useCallback(() => {
+    setOpen(false);
+  }, [])
+
+  const handleCancel = useCallback(() => {
+    setOpen(false);
+  }, [])
   return (
     <>
+      <Button block onClick={showModal}>
+        {t("AppUml.ConfigScript")}
+      </Button>
+      <Modal
+        title={t("AppUml.ConfigScript")}
+        width={800}
+        open={open}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText={t("Confirm")}
+        cancelText={t("Cancel")}
+      >
+      </Modal>
     </>
   )
 })
