3e81f38abbdfb2a0b480013953cad5032258a570
Water.Li <lizongbin@hotmail.com>
2022-10-04 19:57:19 +0800

    Update index.tsx

--- src/plugins/processes/components/pc/ApprovalBar/view/index.tsx
+++ src/plugins/processes/components/pc/ApprovalBar/view/index.tsx
@@ -2,7 +2,6 @@ import { observer } from "@formily/reactive-react"
 import React from "react"
 import cls from "classnames"
 import "./style.less"
-import { Space } from "antd"
 
 export interface IApprovalBarProps {
   className?: string,
@@ -13,10 +12,10 @@ export const ApprovalBar = observer((props: IApprovalBarProps) => {
   const { className, children, ...other } = props;
 
   return (
-    <Space className={cls("appx-arroval-bar", className)} {...other}>
+    <div className={cls("appx-arroval-bar", className)} {...other}>
       {
         children
       }
-    </Space>
+    </div>
   )
 })
