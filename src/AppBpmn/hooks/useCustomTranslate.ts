fe13b82f6829a7a3d1abdaac37b2e43c581af148
Water.Li <lizongbin@hotmail.com>
2022-10-06 15:37:24 +0800

    backup

--- src/AppBpmn/hooks/useCustomTranslate.ts
+++ src/AppBpmn/hooks/useCustomTranslate.ts
@@ -5,6 +5,7 @@ export function useCustomTranslate() {
   const { t } = useTranslation();
 
   const translate = useCallback((template, replacements) => {
+    console.log("翻译遗漏追踪",  t("AppBpmn." + template))
     template = t("AppBpmn." + template) || template;
     return template.replace(/{([^}]+)}/g, function (_, key) {
       return replacements[key] || '{' + key + '}';
