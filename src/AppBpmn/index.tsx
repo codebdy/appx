ae0d2fa42639f52c060f4cc51826f5bc42252101
Water.Li <lizongbin@hotmail.com>
2022-10-08 09:08:44 +0800

    Update index.tsx

--- src/AppBpmn/index.tsx
+++ src/AppBpmn/index.tsx
@@ -41,6 +41,7 @@ export const AppBpmn = memo((props) => {
   const selectedProcessId = useRecoilValue(selectedBpmnProcessIdState(app?.uuid));
   const { process, loading, error } = useQueryOneProcess(selectedProcessId)
   const [minMap, setMinMap] = useRecoilState(minMapState(app?.uuid));
+  const [xml, setXml] = useState<string>();
   const minMapRef = useRef(minMap);
   minMapRef.current = minMap;
   const translate = useCustomTranslate();
@@ -157,7 +158,9 @@ export const AppBpmn = memo((props) => {
           onClick={handleToggleCode}
         >
           <svg style={{ width: '18px', height: '18px', marginTop: "4px" }} viewBox="0 0 24 24">
-            <path fill="currentColor" d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" />
+            <path
+              fill={!!xml && selectedProcessId ? PRIMARY_COLOR : "currentColor"}
+              d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" />
           </svg>
         </Button>
         <Divider type="vertical" />
@@ -213,7 +216,6 @@ export const AppBpmn = memo((props) => {
               </div>
             </div>
           </div>
-
           <div className="canvas" ref={canvasrRef} id="js-canvas"></div>
         </div>
       </Spin>
