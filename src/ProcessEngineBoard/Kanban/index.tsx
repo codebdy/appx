a9cab36b85612216a3a622b95f96c26d5bc63986
Water.Li <lizongbin@hotmail.com>
2022-10-09 10:07:29 +0800

    backup

--- src/ProcessEngineBoard/Kanban/index.tsx
+++ src/ProcessEngineBoard/Kanban/index.tsx
@@ -1,4 +1,4 @@
-import { Card } from "antd"
+import { Card, Progress } from "antd"
 import React from "react"
 import { memo } from "react"
 
@@ -7,10 +7,35 @@ export const Kanban = memo(() => {
     <div className="kanban-layout">
       <div className="process-summary">
         <Card>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
+          <div className="total-title">
+            共有1个正在执行的流程实例
+          </div>
+          <Progress percent={100} />
+          <div className="sub-title">
+            <div>
+              带事件的流程实例
+            </div>
+            <div>
+              活动的流程实例
+            </div>
+          </div>
         </Card>
+      </div>
+      <div className="process-categories">
+        <div className="category-item" style={{ paddingRight: 8 }}>
+          <Card
+            title="按名称分组的流程实例"
+          >
+            ddd
+          </Card>
+        </div>
+        <div className="category-item" style={{ paddingLeft: 8 }}>
+          <Card
+            title="按错误消息分组的流程事件"
+          >
+            sss
+          </Card>
+        </div>
       </div>
     </div>
   )
