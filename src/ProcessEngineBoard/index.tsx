ed9be95340a567fd1dd2f9d652012825fbed99ed
Water.Li <lizongbin@hotmail.com>
2022-10-09 09:28:08 +0800

    backup

--- src/ProcessEngineBoard/index.tsx
+++ src/ProcessEngineBoard/index.tsx
@@ -1,10 +1,37 @@
+import { Card, Tabs } from "antd"
 import React from "react"
 import { memo } from "react"
+import Container from "../plugins/framelayouts/pc/Container/view"
+import "./style.less"
 
-export const ProcessEngineBoard = memo(()=>{
+export const ProcessEngineBoard = memo(() => {
   return (
-    <div>
-      ProcessEngineBoard
+    <div className="process-border-paper">
+      <Tabs
+        defaultActiveKey="2"
+        items={[
+          {
+            label: `配置`,
+            key: '1',
+            children: `Content of Tab Pane 1`,
+          },
+          {
+            label: `概览`,
+            key: '2',
+            children: `Content of Tab Pane 1`,
+          },
+          {
+            label: `流程`,
+            key: '3',
+            children: `Content of Tab Pane 2`,
+          },
+          {
+            label: `决策`,
+            key: '4',
+            children: `Content of Tab Pane 3`,
+          },
+        ]}
+      />
     </div>
   )
 })
