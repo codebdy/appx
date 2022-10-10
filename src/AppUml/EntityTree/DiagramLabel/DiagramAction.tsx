560637dc296cba9a1f2eb5e61a1273f9955fcae4
Water.Li <lizongbin@hotmail.com>
2022-10-10 09:19:24 +0800

    backup

--- src/AppUml/EntityTree/DiagramLabel/DiagramAction.tsx
+++ src/AppUml/EntityTree/DiagramLabel/DiagramAction.tsx
@@ -2,11 +2,11 @@ import { MoreOutlined, EditOutlined, DeleteOutlined, LockOutlined } from "@ant-d
 import { Menu, Dropdown, Button } from "antd";
 import React, { memo, useCallback, useMemo } from "react"
 import { useTranslation } from "react-i18next";
-import { DiagramMeta } from "../meta/DiagramMeta";
-import { useGetPackage } from "../hooks/useGetPackage";
-import { useDeleteDiagram } from "../hooks/useDeleteDiagram";
-import { SYSTEM_APP_UUID } from "../../consts";
-import { useEdittingAppUuid } from "../../hooks/useEdittingAppUuid";
+import { DiagramMeta } from "../../meta/DiagramMeta";
+import { useGetPackage } from "../../hooks/useGetPackage";
+import { useDeleteDiagram } from "../../hooks/useDeleteDiagram";
+import { SYSTEM_APP_UUID } from "../../../consts";
+import { useEdittingAppUuid } from "../../../hooks/useEdittingAppUuid";
 
 const DiagramAction = memo((
   props: {
---
+++ src/AppUml/EntityTree/DiagramLabel/DiagramDialog.tsx
@@ -0,0 +1,64 @@
+import { Form, Modal } from "antd"
+import React, { memo, useCallback, useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
+import { PackageMeta, PackageStereoType } from "../../meta/PackageMeta"
+import { MultiLangInput } from "../../../plugins/inputs/components/pc/MultiLangInput/view"
+import { DiagramMeta } from "../../meta/DiagramMeta"
+
+export const DiagramDialog = memo((
+  props: {
+    open?: boolean,
+    diagram: DiagramMeta,
+    onClose: () => void,
+    onConfirm: (diagram: DiagramMeta) => void,
+  }
+) => {
+  const { open, diagram: diagram, onClose, onConfirm } = props;
+  const [form] = Form.useForm<PackageMeta>();
+  useEffect(() => {
+    form.setFieldsValue(diagram)
+  }, [form, diagram])
+  const { t } = useTranslation();
+
+  const handleConfirm = useCallback(() => {
+    form.validateFields().then(changeValues => {
+      onConfirm({ ...diagram, ...changeValues })
+    })
+  }, [onConfirm, form])
+
+  return (
+    <Modal
+      title={t("AppUml.PackageInfo")}
+      open={open}
+      cancelText={t("Cancel")}
+      okText={t("Confirm")}
+      onCancel={onClose}
+      onOk={handleConfirm}
+      centered
+      wrapProps={
+        {
+          onClick: (e) => {
+            e.stopPropagation()
+          },
+        }
+      }
+    >
+      <Form
+        name="editProcess"
+        labelWrap
+        initialValues={{ title: "", description: "" }}
+        layout="vertical"
+        form={form}
+        autoComplete="off"
+      >
+        <Form.Item
+          label={t("Name")}
+          name="name"
+          rules={[{ required: true, message: t("Required") }]}
+        >
+          <MultiLangInput inline title={t("Name")} />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+})
