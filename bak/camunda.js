a4bf9a84156f91d7c804f80a03915586d69b0b5d
Water.Li <lizongbin@hotmail.com>
2022-10-11 15:38:12 +0800

    Update camunda.js

--- bak/camunda.js
+++ bak/camunda.js
@@ -1,4 +1,18 @@
-goFetch("http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token", 
+goFetch("http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token",
+  {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "client_id=appx&client_secret=bx5Ono2y0FYEVRWB7zw6S55pOEmKQ2kW&grant_type=client_credentials",
+  }
+)
+  .then(result => callback(result))
+  .catch(error => callback('error' + error.message));
+
+
+function getToken() {
+  let result = iFetch("http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token",
     {
       method: 'POST',
       headers: {
@@ -7,5 +21,27 @@ goFetch("http://localhost:18080/auth/realms/camunda-platform/protocol/openid-con
       body: "client_id=appx&client_secret=bx5Ono2y0FYEVRWB7zw6S55pOEmKQ2kW&grant_type=client_credentials",
     }
   )
-  .then(result => callback(result))
-  .catch(error => callback('error'+error.message));
+  return result;
+}
+
+function getProcesses(token) {
+  let result = iFetch("http://localhost:8081/v1/process-instances/search",
+    {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "accept": "application/json",
+        "Authorization": token,
+      },
+      body: JSON.stringify({}),
+    }
+  )
+  return result;
+}
+
+
+const token = getToken()
+
+const tokenObj = JSON.parse(token)
+
+return getProcesses(tokenObj.token_type + " " + tokenObj.access_token)
