4c3e91c2f35561c1b9cb1662c26c8cab341ef612
Water.Li <lizongbin@hotmail.com>
2022-10-11 13:43:47 +0800

    backup

--- src/AppUml/CodeEditor/index.tsx
+++ src/AppUml/CodeEditor/index.tsx
@@ -1,10 +1,20 @@
 import React, { memo, useCallback } from "react"
 import MonacoEditor from "react-monaco-editor"
+import { useSetRecoilState } from "recoil";
+import { useEdittingAppUuid } from "../../hooks/useEdittingAppUuid";
+import { useBackupSnapshot } from "../hooks/useBackupSnapshot";
+import { useSelectedCode } from "../hooks/useSelectedCode";
+import { codesState } from "../recoil/atoms";
 
 export const CodeEditor = memo(() => {
-  const handleChange = useCallback(() => {
-
-  }, [])
+  const appUuid = useEdittingAppUuid();
+  const code = useSelectedCode(appUuid);
+  const setCodes = useSetRecoilState(codesState(appUuid))
+  const backup = useBackupSnapshot(appUuid);
+  const handleChange = useCallback((value: string) => {
+    backup();
+    setCodes(codes => codes.map(cd => cd.uuid === code.uuid ? { ...cd, code: value } : cd))
+  }, [backup, setCodes, code])
   return (
     <div style={{ height: "100%" }}>
       <MonacoEditor
@@ -13,7 +23,7 @@ export const CodeEditor = memo(() => {
           selectOnLineNumbers: true
         }}
         theme={'vs-dark'}
-        value={"value"}
+        value={code?.code}
         //editorDidMount={handleEditorDidMount}
         onChange={handleChange}
       />
---
+++ src/AppUml/hooks/useSelectedCode.ts
@@ -0,0 +1,10 @@
+import { useRecoilValue } from "recoil";
+import { ID } from "../../shared";
+import { codesState, selectedCodeState } from "../recoil/atoms";
+
+export function useSelectedCode(appUuid: ID) {
+  const selectedCodeId = useRecoilValue(selectedCodeState(appUuid));
+  const codes = useRecoilValue(codesState(appUuid));
+
+  return codes.find((code) => code.uuid === selectedCodeId);
+}
