ff34ec8b348c28290c05020ce712c466ca8d5286
Water.Li <lizongbin@hotmail.com>
2022-10-13 18:27:46 +0800

    backup

--- src/AuthBoard/ModelAuthBoard/RoleList.tsx
+++ src/AuthBoard/ModelAuthBoard/RoleList.tsx
@@ -1,10 +1,14 @@
 import React from "react"
 import { memo } from "react"
+import { useTranslation } from "react-i18next"
 
 export const RoleList = memo(() => {
+  const {t} = useTranslation();
   return (
     <div className="right-border role-list">
-      <div>        哈哈</div>
+      <div className="bottom-border roles-title">
+        {t("Auth.Role List")}
+      </div>
       <div className="role-list-body">
 
         <br /> fwe 3
