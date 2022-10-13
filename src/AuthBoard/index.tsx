cd753edb5d58a99cb6924f860cabb3515c9a638e
Water.Li <lizongbin@hotmail.com>
2022-10-13 17:23:25 +0800

    Update index.tsx

--- src/AuthBoard/index.tsx
+++ src/AuthBoard/index.tsx
@@ -2,12 +2,15 @@ import React, { useCallback, useMemo } from "react"
 import { memo } from "react"
 import { Outlet, useNavigate } from "react-router-dom"
 import { ListConentLayout } from "../common/ListConentLayout"
-import { MenuProps } from 'antd';
+import { MenuProps, Spin } from 'antd';
 import { Menu } from 'antd';
 import { AppstoreOutlined, DesktopOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 import "./style.less";
 import SvgIcon from "../common/SvgIcon";
+import { useEdittingAppUuid } from "../hooks/useEdittingAppUuid";
+import { useReadMeta } from "../AppUml/hooks/useReadMeta";
+import { useShowError } from "../hooks/useShowError";
 
 export enum AuthRoutes {
   UiAuth = "ui-auth",
@@ -36,6 +39,11 @@ function getItem(
 export const AuthBoard = memo(() => {
   const { t } = useTranslation();
   const navigate = useNavigate()
+  const appUuid = useEdittingAppUuid();
+  const { loading, error } = useReadMeta(appUuid);
+
+  useShowError(error);
+
   const items: MenuProps['items'] = useMemo(() => [
     getItem(t("Auth.UiAuth"), AuthRoutes.UiAuth, <DesktopOutlined />, []),
     getItem(t("Auth.ModelAuth"),
@@ -53,20 +61,22 @@ export const AuthBoard = memo(() => {
   }, [navigate]);
 
   return (
-    <ListConentLayout
-      className="appx-auth-board"
-      list={
-        <Menu
-          style={{ flex: 1 }}
-          onClick={onClick}
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          items={items}
-        />
-      }
-    >
-      <Outlet />
-    </ListConentLayout>
+    <Spin tip="Loading..." spinning={loading}>
+      <ListConentLayout
+        className="appx-auth-board"
+        list={
+          <Menu
+            style={{ flex: 1 }}
+            onClick={onClick}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            items={items}
+          />
+        }
+      >
+        <Outlet />
+      </ListConentLayout>
+    </Spin>
   )
 })
