80532ced82cc6597e7257738c60d36ecb830af51
Water.Li <lizongbin@hotmail.com>
2022-10-13 18:14:53 +0800

    backup

---
+++ src/AuthBoard/ModelAuthBoard/RoleList.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { memo } from "react"
+
+export const RoleList = memo(()=>{
+  return(
+    <div className="right-border role-list">
+      哈哈
+    </div>
+  )
+})
--- src/AuthBoard/ModelAuthBoard/index.tsx
+++ src/AuthBoard/ModelAuthBoard/index.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { memo } from "react"
 import { ListConentLayout } from "../../common/ListConentLayout"
+import { RoleList } from "./RoleList"
+import "./style.less"
 
 export const ModelAuthBoard = memo(() => {
   return (
     <ListConentLayout
       listWidth={200}
       list={
-        <>哈哈</>
+        <RoleList />
       }
     >
       hehe
