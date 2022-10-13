7c76270a469e1219c55830ff71070ecc9f28b86f
Water.Li <lizongbin@hotmail.com>
2022-10-13 18:38:45 +0800

    backup

--- src/AuthBoard/ModelAuthBoard/RoleList.tsx
+++ src/AuthBoard/ModelAuthBoard/RoleList.tsx
@@ -1,22 +1,47 @@
+import { UserOutlined } from "@ant-design/icons";
+import { List, Menu, MenuProps } from "antd";
 import React from "react"
 import { memo } from "react"
 import { useTranslation } from "react-i18next"
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+const items: MenuProps['items'] = [
+  getItem('Navigation One', 'sub1', <UserOutlined />),
+
+  getItem('Navigation Two', 'sub2', <UserOutlined />),
+
+  getItem('Navigation Three', 'sub4', <UserOutlined />),
+];
+
 export const RoleList = memo(() => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
     <div className="right-border role-list">
       <div className="bottom-border roles-title">
         {t("Auth.Role List")}
       </div>
       <div className="role-list-body">
-
-        <br /> fwe 3
-        <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3
-        <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3
-        <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3
-        <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3
-        <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3      <br /> fwe 3
+        <Menu
+          mode="inline"
+          items={items}
+        />
       </div>
 
     </div>
