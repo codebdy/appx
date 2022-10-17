4833d2300c8a373608457d6d5f4f95c0c01585f8
Water.Li <lizongbin@hotmail.com>
2022-10-17 14:38:52 +0800

    backup

--- src/MonitorCenter/BusinessLogsBoard/index.tsx
+++ src/MonitorCenter/BusinessLogsBoard/index.tsx
@@ -1,10 +1,18 @@
+import { Breadcrumb } from "antd"
 import React from "react"
 import { memo } from "react"
+import { useTranslation } from "react-i18next";
 
 export const BusinessLogsBoard = memo(()=>{
+  const { t } = useTranslation();
+  
   return(
-    <>
-    BusinessLogBoard
-    </>
+    <div className="monitor-board-content">
+      <Breadcrumb className="monitor-breadcrumb">
+        <Breadcrumb.Item>{t("Monitor.Title")}</Breadcrumb.Item>
+        <Breadcrumb.Item>{t("Monitor.Logs")}</Breadcrumb.Item>
+        <Breadcrumb.Item>{t("Monitor.BusinessLogs")}</Breadcrumb.Item>
+      </Breadcrumb>
+    </div>
   )
 })
--- src/MonitorCenter/ModelLogsBoard/index.tsx
+++ src/MonitorCenter/ModelLogsBoard/index.tsx
@@ -1,10 +1,18 @@
+import { Breadcrumb } from "antd";
 import React from "react"
 import { memo } from "react"
+import { useTranslation } from "react-i18next";
 
 export const ModelLogsBoard = memo(()=>{
-  return(
-    <>
-    ModelLogsBoard
-    </>
+  const { t } = useTranslation();
+
+  return (
+    <div className="monitor-board-content">
+      <Breadcrumb className="monitor-breadcrumb">
+        <Breadcrumb.Item>{t("Monitor.Title")}</Breadcrumb.Item>
+        <Breadcrumb.Item>{t("Monitor.Logs")}</Breadcrumb.Item>
+        <Breadcrumb.Item>{t("Monitor.ModelLogs")}</Breadcrumb.Item>
+      </Breadcrumb>
+    </div>
   )
 })
