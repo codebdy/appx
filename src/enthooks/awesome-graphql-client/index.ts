6f4e290d2b0e70c922ea2adbeefce654eb1c8da5
Water.Li <lizongbin@hotmail.com>
2022-10-18 10:29:46 +0800

    未调试通过

--- src/enthooks/awesome-graphql-client/index.ts
+++ src/enthooks/awesome-graphql-client/index.ts
@@ -3,6 +3,4 @@ import { GraphQLRequestError } from './GraphQLRequestError'
 import { gql } from './util/gql'
 import { isFileUpload, FileUpload } from './util/isFileUpload'
 
-console.log("呵呵呵", AwesomeGraphQLClient, gql)
-
 export { AwesomeGraphQLClient, GraphQLRequestError, gql, isFileUpload, FileUpload }
