bf916332fb2e23b72581a56af24ebcde04ab4954
Water.Li <lizongbin@hotmail.com>
2022-10-28 14:09:27 +0800

    Update index.tsx

--- src/plugins/arrays/components/pc/GridList/designer/index.tsx
+++ src/plugins/arrays/components/pc/GridList/designer/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import {
   DnFC,
   useTreeNode,
+  TreeNodeWidget,
 } from '@designable/react'
 import { observer } from '@formily/reactive-react'
 import './styles.less'
@@ -9,16 +10,31 @@ import { IGridListProps } from '../view'
 import cls from "classnames";
 import { IListHeaderProps } from '../view/ListHeader'
 import { ListHeaderDesigner } from './ListHeaderDesigner'
+import { useFindNode } from '~/plugin-sdk'
 
 export const GridListDesigner: DnFC<IGridListProps> & {
   Header?: React.FC<IListHeaderProps>
-}   = observer((props) => {
+} = observer((props) => {
   const { className, hasHeader, pagination, paginationPosition, pageSize, ...other } = props;
   const node = useTreeNode()
+  const header = useFindNode('Header');
+  const otherChildrenNodes = useMemo(() => node.children?.filter(child =>
+    child.id !== header?.id
+  ), [header?.id, node.children])
+
 
   return (
     <div {...other} className={cls("appx-grid-list", className)}>
-
+      {
+        hasHeader && header && <TreeNodeWidget node={header} />
+      }
+      {
+        otherChildrenNodes?.map((child) => {
+          return (
+            child && <TreeNodeWidget node={child} />
+          )
+        })
+      }
     </div>
   )
 })
