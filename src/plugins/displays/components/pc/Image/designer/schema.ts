004fe335cf0cfa32cfdf74bbdb7f72f6317ef5dd
Water.Li <lizongbin@hotmail.com>
2022-10-28 18:33:53 +0800

    backup

--- src/plugins/displays/components/pc/Image/designer/schema.ts
+++ src/plugins/displays/components/pc/Image/designer/schema.ts
@@ -7,28 +7,7 @@ const schema: IPropsSchema = {
   props: {
     type: 'object',
     properties: {
-      icon: {
-        type: 'string',
-        'x-decorator': 'FormItem',
-        'x-component': 'IconInput',
-        'x-component-props': {
-        }
-      },
-      size: {
-        type: 'number',
-        'x-decorator': 'FormItem',
-        'x-component': 'NumberPicker',
-      },
-      shape: {
-        type: 'string',
-        enum: ['circle', 'square'],
-        'x-decorator': 'FormItem',
-        'x-component': 'Radio.Group',
-        'x-component-props': {
-          defaultValue: 'circle',
-          optionType: 'button',
-        },
-      }
+
     },
   }
 }
--- src/plugins/displays/components/pc/Image/view/index.tsx
+++ src/plugins/displays/components/pc/Image/view/index.tsx
@@ -3,14 +3,24 @@ import React, { CSSProperties } from "react"
 import "./style.less"
 
 export interface IImageProps {
+  empertyIconSize?: number,
   value?: string,
   style?: CSSProperties,
 }
 
 export const Image = observer((props: IImageProps) => {
-  const { value, style, ...other } = props;
+  const { empertyIconSize= 120, value, style, ...other } = props;
   return (
-    <div className="app-image" style={{ ...style, backgroundImage: `url(${value})` }} {...other}>
+    <div className="appx-image" style={{ ...style, backgroundImage: `url(${value})` }} {...other}>
+      {
+        !value &&
+        <div className="no-image-mask">
+          <svg style={{ width: empertyIconSize, height: empertyIconSize }} viewBox="0 0 24 24">
+            <path fill="currentColor" d="M23 11.5L19.95 10.37C19.69 9.22 19.04 8.56 19.04 8.56C17.4 6.92 14.75 6.92 13.11 8.56L11.63 10.04L5 3C4 7 5 11 7.45 14.22L2 19.5C2 19.5 10.89 21.5 16.07 17.45C18.83 15.29 19.45 14.03 19.84 12.7L23 11.5M17.71 11.72C17.32 12.11 16.68 12.11 16.29 11.72C15.9 11.33 15.9 10.7 16.29 10.31C16.68 9.92 17.32 9.92 17.71 10.31C18.1 10.7 18.1 11.33 17.71 11.72Z" />
+          </svg>
+        </div>
+      }
+
     </div>
   )
 })
