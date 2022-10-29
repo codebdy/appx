b97b067ac8f9e9d542809f6cf2f51fdde882c12e
Water.Li <lizongbin@hotmail.com>
2022-10-29 10:50:28 +0800

    Update ListBody.tsx

--- src/plugins/arrays/components/pc/GridList/view/ListBody.tsx
+++ src/plugins/arrays/components/pc/GridList/view/ListBody.tsx
@@ -1,9 +1,12 @@
 import { memo } from "react"
 import { Empty, List } from 'antd';
 import React from "react";
-import { RecursionField } from '@formily/react';
+import { RecursionField, useFieldSchema } from '@formily/react';
 import { useParseLangMessage } from "~/plugin-sdk";
-const data = [{}];
+import { useArrayParams } from "~/plugin-sdk/contexts/array";
+import { QueryType, useQueryParams } from "~/datasource/hooks/useQueryParams";
+import { useDataQuery } from "~/datasource";
+import { useShowError } from "~/AppDesigner/hooks/useShowError";
 
 export interface IGrid {
   column?: number,
@@ -16,19 +19,46 @@ export interface IGrid {
 }
 
 export const ListBody = memo((
-  props:{
+  props: {
     gutter?: number,
     grid?: IGrid,
-    childrenNodes?:any[]
+    childrenNodes?: any[]
   }
 ) => {
-  const {gutter, grid, childrenNodes} = props;
+  const { gutter, grid, childrenNodes } = props;
   const p = useParseLangMessage();
-  
+
+  const {
+    dataBind,
+    queryForm,
+    pageSize,
+    current,
+    orderBys,
+    refreshFlag,
+  } = useArrayParams();
+
+  const schema = useFieldSchema();
+  const queryParams = useQueryParams(
+    dataBind,
+    schema,
+    QueryType.Multiple,
+    {
+      queryForm,
+      orderBys,
+      current,
+      pageSize,
+      refreshFlag,
+    },
+  );
+
+  const { data, loading, error } = useDataQuery(queryParams);
+  useShowError(error);
+
   return (
     <List
       grid={{ gutter, ...grid }}
       dataSource={data}
+      loading={loading}
       renderItem={item => (
         <List.Item>
           {
