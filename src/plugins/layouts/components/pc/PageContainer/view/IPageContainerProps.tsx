310128e929f46c61e6324925beeb6f77f338835d
Water.Li <lizongbin@hotmail.com>
2022-11-03 17:56:59 +0800

    Update IPageContainerProps.tsx

--- src/plugins/layouts/components/pc/PageContainer/view/IPageContainerProps.tsx
+++ src/plugins/layouts/components/pc/PageContainer/view/IPageContainerProps.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { IDataSourceableProps } from "@rxdrag/plugin-sdk/model/IDataSourceableProps";
 
-export interface IPageContainerProps extends IDataSourceableProps{
+export interface IPageContainerProps extends IDataSourceableProps {
   title?: string;
   subtitle?: string;
   children?: React.ReactNode;
   className?: string;
+  style?: CSSProperties;
   hasBreadcrumb?: boolean;
   hasGobackButton?: boolean;
   hasActions?: boolean;
