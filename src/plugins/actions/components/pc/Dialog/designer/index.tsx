8cf753bc7be1e8900c060f724fd3bff407801702
Water.Li <lizongbin@hotmail.com>
2022-11-07 13:29:37 +0800

    backup

--- src/plugins/actions/components/pc/Dialog/designer/index.tsx
+++ src/plugins/actions/components/pc/Dialog/designer/index.tsx
@@ -1,13 +1,9 @@
 import React, {
-  createContext,
-  useContext,
   useState,
-  useImperativeHandle,
   useRef,
-  useLayoutEffect,
-  useEffect, useMemo,
+  useEffect,
 } from 'react'
-import { Button, Modal as AntdModal, ModalProps, Space } from 'antd'
+import { Button, Modal as AntdModal, ModalProps } from 'antd'
 
 import { TreeNode } from '@designable/core'
 import {
@@ -20,12 +16,17 @@ import {
   useTreeNode,
   useSelected
 } from '@designable/react'
-import { observer, RecursionField, Schema, useField, useFieldSchema } from '@formily/react'
+import { observer, Schema } from '@formily/react'
 import { Field, GeneralField } from '@formily/core'
 import './styles.less'
-import { define, observable } from '@formily/reactive'
-import { Locale } from 'antd/es/locale-provider'
 import { findNodeByComponentPath } from '~/plugin-sdk'
+import Dialog from '../view'
+import { IDialogTitleProps } from '../view/DialogTitle'
+import { IDialogContentProps } from '../view/DialogContent'
+import { IDialogFooterProps } from '../view/DialogFooter'
+import { DialogTitleDesigner } from './DialogTitleDesigner'
+import { DialogContentDesigner } from './DialogContentDesigner'
+import { DialogFooterDesigner } from './DialogFooterDesigner'
 
 
 export interface IDialogProps extends ModalProps, IDialogRef {
@@ -51,162 +52,12 @@ export interface IDialogRef {
   context: IDialogContext
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export const DialogContext = createContext<IDialogContext>()
 
-export const getDialog = (key: string) => {
-
-  if (key && DialogMap.has(key)) {
-    return DialogMap.get(key)
-  }
-  return null
-}
-
-export const useDialog = (key?: string) => {
-
-  if (key && DialogMap.has(key)) {
-    return DialogMap.get(key)
-  }
-  return useContext(DialogContext)
-
-}
-
-const DialogMap = new Map<any, IDialogContext>()
-
-const isFooterComponent = (schema: Schema) => {
-  return schema['x-component']?.indexOf('Dialog.Footer') > -1
-}
-const isBodyComponent = (schema: Schema) => {
-  return schema['x-component']?.indexOf('Dialog.Body') > -1
-}
-
-const useBody = (schema: Schema) => {
-  if (!schema) return null
-  return schema.reduceProperties((acc, sc, key) => {
-    if (isBodyComponent(sc)) {
-      return <RecursionField schema={sc} key={key} />
-    }
-    return acc
-  }, null)
-}
-
-const useFooter = (schema: Schema) => {
-  if (!schema) return null
-  return schema.reduceProperties((acc, sc, key) => {
-    if (isFooterComponent(sc)) {
-      return <RecursionField schema={sc} key={key} />
-    }
-    return acc
-  }, null)
-}
-
-type IDialogBody = IDialogProps & { schema: Schema }
-type IDialogFooter = IDialogProps & { schema: Schema }
-
-const DialogFooter :any = ({ schema, ...props }) => {
-
-  const footer = (locale: Locale['Modal']) => {
-
-    const f = useFooter(schema)
-    const defaultFooter = props.footer ?? (<>
-      <Button onClick={props.onCancel} {...props.cancelButtonProps}>{props.cancelText ?? locale.cancelText}</Button>
-      <Button type={props.okType as any ?? 'primary'}
-        onClick={props.onOk} {...props.okButtonProps}>{props.okText ?? locale.okText}</Button>
-    </>)
-
-    return <Space>
-      {
-        f || defaultFooter
-      }
-    </Space>
-  }
-
-  return (
-     <></> 
-  )
-}
-
-const DialogBody: React.FC<IDialogBody> = ({ schema, ...props }) => {
-
-  const body = useBody(schema) ?? props.children
-  return body
-}
-
-export const BaseDialog: React.FC<IDialogProps> & {
-  Body?: React.FC<any>,
-  Footer?: React.FC<any>,
-} = observer((
-  {
-    target,
-    ref,
-    onCancel,
-    ...props
-  }) => {
-
-  const field = useField()
-  const schema = useFieldSchema()
-  const dialog = useDialog()
-
-  const contextValue = useMemo(() => define({
-    ...dialog,
-    field,
-    schema,
-    visible: false,
-    id: field.address.toString(),
-  }, {
-    visible: observable.ref
-  }), [])
-
-  useEffect(() => {
-
-    if (field) {
-      if (!DialogMap.has(field.address.toString())) {
-        DialogMap.set(field.address.toString(), contextValue)
-      }
-    }
-    return () => {
-      if (field) {
-        DialogMap.delete(field.address.toString())
-      }
-    }
-  }, [field])
-
-  useImperativeHandle<IDialogProps, IDialogRef>(ref, () => ({
-    context: contextValue,
-  }), [])
-
-  const handelCancel = (e) => {
-    if (onCancel && onCancel(e) === false) {
-      return
-    }
-    contextValue.visible = false
-  }
-
-  return (
-    <DialogContext.Provider value={contextValue}>
-      {target}
-      <AntdModal
-        {...props}
-        open={contextValue.visible}
-        footer={<DialogFooter {...props} schema={schema} onCancel={handelCancel} />}
-        onCancel={handelCancel}
-      >
-        <DialogBody {...props} schema={schema} />
-      </AntdModal>
-    </DialogContext.Provider>
-  )
-}, { forwardRef: true })
-
-BaseDialog.Footer = () => {
-  return <React.Fragment />
-}
-
-BaseDialog.Body = () => {
-  return <React.Fragment />
-}
-
-export const DialogDesigner: DnFC<React.ComponentProps<typeof BaseDialog>> = observer((props) => {
+export const DialogDesigner: DnFC<IDialogProps> & {
+  Title?: React.FC<IDialogTitleProps>,
+  Content?: React.FC<IDialogContentProps>,
+  Footer?: React.FC<IDialogFooterProps>,
+} = observer((props) => {
   const dialog = useRef<IDialogRef>()
   const tree = useTree()
   const designer = useDesigner()
@@ -219,9 +70,7 @@ export const DialogDesigner: DnFC<React.ComponentProps<typeof BaseDialog>> = obs
 
   const canShow = useRef(selected?.[0] === node.id ? 1 : 0)
 
-  useLayoutEffect(() => {
-    dialogContainer.current = document.querySelector('.dn-designable-form')
-  }, [])
+  dialogContainer.current = document.querySelector('.dn-designable-form')
 
   useEffect(() => {
     if (!dialogContainer.current) {
@@ -334,38 +183,45 @@ export const DialogDesigner: DnFC<React.ComponentProps<typeof BaseDialog>> = obs
           setVisible(true)
           dialog.current!.context!.visible = true
         }}>编辑弹窗内容</Button>
-      <BaseDialog
-        {...dialogProps}
-        title={
-          <span data-content-editable="x-component-props.title">
-            {props.title}
-          </span>
-        }
-        ref={dialog}
-        closable={true}
-        data-dialog-id={nodeIdProps[designer.props.nodeIdAttrName]}
-        getContainer={() => dialogContainer.current}
-        maskClosable={false}
-        transitionName={''}
-        // footer={
-        //   footer?.children.length > 0 ? <TreeNodeWidget node={footer} key={footer?.id} /> :
-        //     <DroppableWidget node={footer} />
-        // }
-        onCancel={() => {
-          setVisible(false)
-          dialog.current!.context!.visible = false
-        }}>
-        {
-          visible && <>
-            {
-              body?.children.length > 0 ? <TreeNodeWidget node={body} key={body?.id} /> : <DroppableWidget node={body} />
-            }
+      {
+        visible &&
+        <AntdModal
+          {...dialogProps}
+          open={visible}
+          title={
+            <span data-content-editable="x-component-props.title">
+              {props.title}
+            </span>
+          }
+          closable={true}
+          data-dialog-id={nodeIdProps[designer.props.nodeIdAttrName]}
+          getContainer={() => dialogContainer.current}
+          maskClosable={false}
+          transitionName={''}
+          // footer={
+          //   footer?.children.length > 0 ? <TreeNodeWidget node={footer} key={footer?.id} /> :
+          //     <DroppableWidget node={footer} />
+          // }
+          onCancel={() => {
+            setVisible(false)
+            dialog.current!.context!.visible = false
+          }}>
+          {
+            visible && <>
+              {
+                body?.children.length > 0 ? <TreeNodeWidget node={body} key={body?.id} /> : <DroppableWidget node={body} />
+              }
 
-          </>
-        }
-      </BaseDialog>
+            </>
+          }
+        </AntdModal>
+      }
+
     </>
   )
 })
 
 
+DialogDesigner.Title = DialogTitleDesigner;
+DialogDesigner.Content = DialogContentDesigner;
+DialogDesigner.Footer = DialogFooterDesigner;
