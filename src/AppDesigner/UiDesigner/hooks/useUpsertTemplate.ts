8f8d2aa5eb3f5c00a294269436d9caa2876d997c
Water.Li <lizongbin@hotmail.com>
2022-11-08 18:25:05 +0800

    backup

---
+++ src/AppDesigner/UiDesigner/hooks/useUpsertTemplate.ts
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+import { IPostOptions, usePostOne } from "~/enthooks/hooks/usePostOne";
+import { ITemplateInfo, ITemplateInfoInput } from "~/model";
+import { useDesignerParams } from "~/plugin-sdk/contexts/desinger";
+
+export function useUpsertTemplate(options?: IPostOptions<any>): [
+  (template: ITemplateInfoInput) => void,
+  { loading?: boolean; error?: Error }
+] {
+  const params = useDesignerParams();
+
+  const [post, { error, loading }] = usePostOne<ITemplateInfoInput, ITemplateInfo>("Page",
+    {
+      ...options,
+      fieldsGql: " name"
+    }
+  )
+
+  const upsert = useCallback((page: ITemplateInfoInput) => {
+    post({
+      ...page,
+      device: params.device,
+      app: {
+        sync: { id: params.app.id }
+      },
+    })
+  }, [params.app.id, params.device, post]);
+
+
+  return [upsert, { error: error, loading: loading }]
+}
