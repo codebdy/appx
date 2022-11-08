21f96483861a30787654a29b6394515f3f01147f
Water.Li <lizongbin@hotmail.com>
2022-11-08 18:05:04 +0800

    Update template.ts

--- src/model/template.ts
+++ src/model/template.ts
@@ -1,6 +1,6 @@
 import { Device } from "@rxdrag/appx-plugin-sdk";
 import { ID } from "~/shared";
-import { IApp } from "./app";
+import { IApp, IAppInput } from "./app";
 
 export enum TemplateType {
   Public = "Public",
@@ -8,13 +8,25 @@ export enum TemplateType {
 }
 
 export interface ITemplateInfo {
-  id?: ID;
+  id: ID;
   name?: string;
   uuid?: string;
   imageUrl?: string;
   dependencies?: any;
   schemaJson?: any;
   app?: IApp;
+  device?: Device;
+  type?: TemplateType;
+}
+
+export interface ITemplateInfoInput {
+  id?: ID;
+  name?: string;
+  uuid?: string;
+  imageUrl?: string;
+  dependencies?: any;
+  schemaJson?: any;
+  app?: { sync?: IAppInput };
   device?: Device;
   type?: TemplateType;
 }
