272ec441d7b8acf2941c9932eef8a85611332412
Water.Li <lizongbin@hotmail.com>
2022-11-09 14:39:17 +0800

    backup

--- src/AppDesigner/UiDesigner/widgets/TemplateWidget/ManageDialog/EditTemplateDialog.tsx
+++ src/AppDesigner/UiDesigner/widgets/TemplateWidget/ManageDialog/EditTemplateDialog.tsx
@@ -1,8 +1,12 @@
 import { EditOutlined } from "@ant-design/icons"
-import { Button } from "antd"
-import React from "react"
+import { Button, Form, Modal } from "antd"
+import React, { useCallback, useState } from "react"
 import { memo } from "react"
+import { useTranslation } from "react-i18next"
+import { useShowError } from "~/AppDesigner/hooks/useShowError"
+import { useUpsertTemplate } from "~/AppDesigner/UiDesigner/hooks/useUpsertTemplate"
 import { ITemplateInfo } from "~/model"
+import { SaveTemplateForm } from "../../SaveTemplateWidget/SaveTemplateForm"
 
 export const EditTemplateDialog = memo((
   props: {
@@ -10,9 +14,56 @@ export const EditTemplateDialog = memo((
   }
 ) => {
   const { template } = props;
+  const [open, setOpen] = useState(false);
+  const { t } = useTranslation();
+  const [form] = Form.useForm()
+  const [upsert, { error, loading }] = useUpsertTemplate({
+    onCompleted: () => {
+      form.resetFields();
+      setOpen(false);
+    }
+  });
+
+  useShowError(error)
+
+  const handleShowModal = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const handleOk = useCallback(() => {
+    form.validateFields().then((values: any) => {
+      upsert({
+        ...template,
+        ...values,
+      })
+    })
+
+  }, [upsert, template]);
+
+  const handleCancel = useCallback(() => {
+    form.resetFields();
+    setOpen(false);
+  }, [form]);
+
   return (
     <>
-      <Button type="text" shape="circle" icon={<EditOutlined />}></Button>
+      <Button type="text" shape="circle" icon={<EditOutlined />} onClick={handleShowModal}></Button>
+      {
+        open && <Modal
+          title={t("Designer.EditTemplate")}
+          open={open}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          okText={t("Confirm")}
+          cancelText={t("Cancel")}
+          okButtonProps={{
+            loading: loading
+          }}
+        >
+          <SaveTemplateForm form={form} />
+        </Modal>
+      }
+
     </>
   )
 })
