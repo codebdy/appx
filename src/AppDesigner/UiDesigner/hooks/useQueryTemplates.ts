5f44a3238a71b8836e30b56ffa42fef4e95d2ca5
Water.Li <lizongbin@hotmail.com>
2022-11-09 11:15:35 +0800

    Create useQueryTemplates.ts

---
+++ src/AppDesigner/UiDesigner/hooks/useQueryTemplates.ts
@@ -0,0 +1,59 @@
+import { gql } from "~/enthooks";
+import { useMemo } from "react";
+import { useQuery } from "~/enthooks/hooks/useQuery";
+import { IPage, ITemplateInfo, TemplateType } from "~/model";
+import { useDesignerParams } from "~/plugin-sdk/contexts/desinger";
+
+const templatesGql = gql`
+query ($appId:ID!, $device:String!, $templateType:String!){
+  templateInfos(where:{
+    _and:[
+      {
+        app:{
+          id:{
+            _eq:$appId
+          }
+        }
+      },
+      {
+        device:{
+          _eq:$device
+        }
+      },
+      {
+        templateType:{
+          _eq:$templateType
+        }
+      }
+    ]
+  },
+  orderBy:{
+    id:asc
+  }
+ ){
+    nodes{
+      id
+      device
+      title
+      categoryUuid
+      uuid
+    }
+  }
+}
+`
+
+export function useQueryTemplates(templateType: TemplateType) {
+  const appParams = useDesignerParams();
+
+  const args = useMemo(() => {
+    return {
+      gql: templatesGql,
+      params: { device: appParams.device, appId: appParams.app.id, templateType },
+      depEntityNames: ["TemplateInfo"]
+    }
+  }, [appParams, templateType])
+
+  const { data, error, loading } = useQuery<ITemplateInfo>(args)
+
+  return { templates: data?.templateInfos?.nodes, error, loading }
+}
