7f617d96e2deaa36ee3ecb634006c7c29b2978dd
Water.Li <lizongbin@hotmail.com>
2022-11-10 13:54:45 +0800

    backup

--- src/AppDesigner/AppUml/hooks/useExportJson.ts
+++ src/AppDesigner/AppUml/hooks/useExportJson.ts
@@ -1,29 +1,23 @@
+import { message } from "antd";
 import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
 import { MetaContent } from "../meta";
+import { saveFile } from "./helper/saveFile";
 import { useGetMeta } from "./useGetMeta";
 
-export const downloadFile = function (filename: string, content: string) {
-  // 创建隐藏的可下载链接
-  var eleLink = document.createElement('a');
-  eleLink.download = filename;
-  eleLink.style.display = 'none';
-  // 字符内容转变成blob地址
-  var blob = new Blob([content]);
-  eleLink.href = URL.createObjectURL(blob);
-  // 触发点击
-  document.body.appendChild(eleLink);
-  eleLink.click();
-  // 然后移除
-  document.body.removeChild(eleLink);
-};
-
-
 export function useExportJson(appId: string) {
+  const { t } = useTranslation();
   const getMeta = useGetMeta(appId)
   const doExport = useCallback(() => {
 
-    const data: MetaContent =getMeta();
-    downloadFile(appId + '.json', JSON.stringify(data, null, 2));
+    const data: MetaContent = getMeta();
+    saveFile(appId + '-model', JSON.stringify(data, null, 2)).then(
+      (savedName) => {
+        if (savedName) {
+          message.success(t("OperateSuccess"))
+        }
+      }
+    );
   }, [appId, getMeta]);
 
   return doExport
