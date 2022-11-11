8eaa0f98ecc2e601c8a11b0f313d803617f2a513
Water.Li <lizongbin@hotmail.com>
2022-11-11 12:11:39 +0800

    backup

--- src/AppDesigner/AppUml/PropertyPanel/AttributeTypeInput.tsx
+++ src/AppDesigner/AppUml/PropertyPanel/AttributeTypeInput.tsx
@@ -1,16 +1,11 @@
 import React, { useMemo } from "react";
-import { Form, Select } from "antd";
+import { Form } from "antd";
 import { memo } from "react";
-import { useEntities } from "../hooks/useEntities";
-import { useEnums } from "../hooks/useEnums";
-import { useValueObjects } from "../hooks/useValueObjects";
-import { Types } from "../meta/Type";
 import { useTranslation } from "react-i18next";
 import { AttributeMeta } from "../meta/AttributeMeta";
 import { CONST_ID } from "../meta/Meta";
-import { useEdittingAppId } from "~/AppDesigner/hooks/useEdittingAppUuid";
 import { TypeSelect } from "./TypeSelect";
-const { Option } = Select;
+import { TypeUuidSelect } from "./TypeUuidSelect";
 
 export const AttributeTypeInput = memo(
   (
@@ -19,10 +14,6 @@ export const AttributeTypeInput = memo(
     }
   ) => {
     const { attribute } = props;
-    const appId = useEdittingAppId();
-    const enums = useEnums(appId);
-    const valueObjects = useValueObjects(appId);
-    const entities = useEntities(appId);
     const { t } = useTranslation();
     const isId = useMemo(() => attribute.name === CONST_ID, [attribute.name]);
 
@@ -34,60 +25,7 @@ export const AttributeTypeInput = memo(
         >
           <TypeSelect disabled={isId}></TypeSelect>
         </Form.Item>
-        {(attribute.type === Types.Enum ||
-          attribute.type === Types.EnumArray) && (
-            <Form.Item
-              label={t("AppUml.EnumClass")}
-              name="typeUuid"
-            >
-              <Select>
-                <Option key="" value="">
-                  <em>None</em>
-                </Option>
-                {enums.map((enumEntity) => {
-                  return (
-                    <Option key={enumEntity.uuid} value={enumEntity.uuid}>{enumEntity.name}</Option>
-                  );
-                })}
-              </Select>
-            </Form.Item>
-          )}
-        {(attribute.type === Types.ValueObject ||
-          attribute.type === Types.ValueObjectArray) && (
-            <Form.Item
-              label={t("AppUml.ValueClass")}
-              name="typeUuid"
-            >
-              <Select>
-                <Option key="" value="">
-                  <em>None</em>
-                </Option>
-                {valueObjects.map((interfaceEntity) => {
-                  return (
-                    <Option key={interfaceEntity.uuid} value={interfaceEntity.uuid}>{interfaceEntity.name}</Option>
-                  );
-                })}
-              </Select>
-            </Form.Item>
-          )}
-        {(attribute.type === Types.Entity ||
-          attribute.type === Types.EntityArray) && (
-            <Form.Item
-              label={t("AppUml.EntityClass")}
-              name="typeUuid"
-            >
-              <Select>
-                <Option key="" value="">
-                  <em>None</em>
-                </Option>
-                {entities.map((entity) => {
-                  return (
-                    <Option key={entity.uuid} value={entity.uuid}>{entity.name}</Option>
-                  );
-                })}
-              </Select>
-            </Form.Item>
-          )}
+        <TypeUuidSelect type={attribute.type} withFormItem />
       </>
     );
   }
---
+++ src/AppDesigner/AppUml/PropertyPanel/TypeUuidSelect.tsx
@@ -0,0 +1,76 @@
+import { Form, Select } from "antd";
+import React, { useMemo } from "react"
+import { memo } from "react"
+import { useTranslation } from "react-i18next";
+import { useEdittingAppId } from "~/AppDesigner/hooks/useEdittingAppUuid";
+import { useEntities } from "../hooks/useEntities";
+import { useEnums } from "../hooks/useEnums";
+import { useValueObjects } from "../hooks/useValueObjects";
+import { Type, Types } from "../meta"
+const { Option } = Select;
+
+export const TypeUuidSelect = memo((
+  props: {
+    type?: Type,
+    withFormItem?: boolean,
+  }
+) => {
+  const { type, withFormItem } = props;
+  const appId = useEdittingAppId();
+  const enums = useEnums(appId);
+  const valueObjects = useValueObjects(appId);
+  const entities = useEntities(appId);
+  const { t } = useTranslation();
+
+  const [title, classes] = useMemo(() => {
+    if (type === Types.Enum ||
+      type === Types.EnumArray) {
+      return [t("AppUml.EnumClass"), enums]
+    }
+
+    if (type === Types.ValueObject ||
+      type === Types.ValueObjectArray) {
+      return [t("AppUml.ValueClass"), valueObjects]
+    }
+
+    if (type === Types.Entity ||
+      type === Types.EntityArray) {
+      return [t("AppUml.EntityClass"), entities]
+    }
+
+    return []
+  }, [type, enums, valueObjects, entities])
+
+  return (
+    classes && (
+      withFormItem
+        ?
+        <Form.Item
+          label={title}
+          name="typeUuid"
+        >
+          <Select>
+            <Option key="" value="">
+              <em>None</em>
+            </Option>
+            {classes.map((cls) => {
+              return (
+                <Option key={cls.uuid} value={cls.uuid}>{cls.name}</Option>
+              );
+            })}
+          </Select>
+        </Form.Item>
+        :
+        <Select>
+          <Option key="" value="">
+            <em>None</em>
+          </Option>
+          {classes.map((cls) => {
+            return (
+              <Option key={cls.uuid} value={cls.uuid}>{cls.name}</Option>
+            );
+          })}
+        </Select>
+    )
+  )
+})
