9aca00ab2a1d9090fbffb5f6da4040721a96855b
Water.Li <lizongbin@hotmail.com>
2022-11-16 09:59:55 +0800

    Update useSubscribeNotificationCounts.ts

--- src/plugins/framewidgets/hooks/useSubscribeNotificationCounts.ts
+++ src/plugins/framewidgets/hooks/useSubscribeNotificationCounts.ts
@@ -1,25 +1,27 @@
 import { createClient } from 'graphql-ws';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { SERVER_SUBSCRIPTION_URL } from '~/consts';
 
 export function useSubscribeNotificationCounts() {
-
+  const [error, setError] = useState<Error>();
+  const [count, setCount] = useState<number>();
   const onNext = useCallback((value) => {
-
+    setCount(value)
   }, [])
 
-  const client = createClient({
-    url: 'ws://localhost:4000/graphql',
-  });
-
   const handleError = useCallback((error) => {
-
+    setError(error)
   }, [])
 
   const handleComplate = useCallback(() => {
-
+    console.log("subscription unreadNoticationCounts complated")
   }, [])
 
   useEffect(() => {
+    const client = createClient({
+      url: SERVER_SUBSCRIPTION_URL,
+    });
+
     const unsubscribe = client.subscribe(
       {
         query: 'subscription { unreadNoticationCounts }',
@@ -33,4 +35,6 @@ export function useSubscribeNotificationCounts() {
     return unsubscribe
   }, [onNext, handleError, handleComplate])
 
+
+  return {count, error}
 }
