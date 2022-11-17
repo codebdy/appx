3b312de61bcb926e9dfa8dd3c2c75d8b8a74d055
Water.Li <lizongbin@hotmail.com>
2022-11-17 14:14:59 +0800

    backup

--- src/plugins/framewidgets/pc/Notification/view/NotificationBox.tsx
+++ src/plugins/framewidgets/pc/Notification/view/NotificationBox.tsx
@@ -1,9 +1,45 @@
+import { SettingOutlined } from "@ant-design/icons"
+import { Button, Card, List, Tabs } from "antd"
 import React from "react"
 
-export const NotificationBox = ()=>{
-  return(
-    <div>
-      哈哈哈
-    </div>
+const data = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
+
+export const NotificationBox = () => {
+  return (
+    <Card
+      className="notificaiton-box float"
+      actions={[
+        <div className="actions">
+          <Button type="text" icon={<SettingOutlined />}>设置</Button>
+          <Button type="text" >查看更多消息</Button>
+        </div>
+      ]}
+    >
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={item => (
+          <List.Item>
+            {item.title}
+            <List.Item.Meta
+              description="20分钟前"
+            />
+          </List.Item>
+        )}
+      />
+    </Card>
   )
 }
