2df8c2dcadaf053669b96e698592fa2f6fa81ecd
Water.Li <lizongbin@hotmail.com>
2022-11-18 17:13:42 +0800

    Update index.tsx

--- src/plugins/processes/components/pc/ApprovalBar/view/index.tsx
+++ src/plugins/processes/components/pc/ApprovalBar/view/index.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import cls from "classnames"
 import "./style.less"
 import { Button, Space, Typography } from "antd"
+import { CheckOutlined, CloseOutlined, MoreOutlined } from "@ant-design/icons"
 
 export interface IApprovalBarProps {
   className?: string,
@@ -16,15 +17,17 @@ export const ApprovalBar = observer((props: IApprovalBarProps) => {
     <div className={cls("appx-arroval-bar", className)} {...other}>
       <Space direction="vertical" size="large">
         <div style={{ display: "flex", flexFlow: "column", alignItems: "center" }}>
-          <Typography.Text type="secondary">状态:</Typography.Text>
-          <Typography.Title level={3} style={{ margin: 0 }}>
-            未启动
-          </Typography.Title>
+          <div>
+            <Typography.Text type="secondary">状态</Typography.Text>
+            <Typography.Title level={3} style={{ margin: 0 }}>
+              未启动
+            </Typography.Title>
+          </div>
         </div>
         <Space>
-          <Button>通过</Button>
-          <Button>驳回</Button>
-          <Button>更多操作</Button>
+          <Button icon={<CloseOutlined />}>驳回</Button>
+          <Button type="primary" icon={<CheckOutlined />}>通过</Button>
+          <Button icon={<MoreOutlined />}></Button>
         </Space>
       </Space>
     </div>
