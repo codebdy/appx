0f7e1edaad31df00a40d32c44d114e9065698cce
Water.Li <lizongbin@hotmail.com>
2022-11-19 09:44:00 +0800

    backup

--- src/plugins/processes/components/pc/ApprovalBar/view/index.tsx
+++ src/plugins/processes/components/pc/ApprovalBar/view/index.tsx
@@ -1,9 +1,10 @@
 import { observer } from "@formily/reactive-react"
-import React from "react"
+import React, { useMemo } from "react"
 import cls from "classnames"
 import "./style.less"
-import { Button, Space, Typography } from "antd"
-import { CheckOutlined, CloseOutlined, MoreOutlined } from "@ant-design/icons"
+import { Button, Dropdown, Menu, Space, Typography } from "antd"
+import { CheckOutlined, ClockCircleOutlined, CloseOutlined, FileAddOutlined, HistoryOutlined, MoreOutlined, NodeIndexOutlined, QuestionCircleOutlined, UserAddOutlined } from "@ant-design/icons"
+import { useTranslation } from "react-i18next"
 
 export interface IApprovalBarProps {
   className?: string,
@@ -12,6 +13,51 @@ export interface IApprovalBarProps {
 
 export const ApprovalBar = observer((props: IApprovalBarProps) => {
   const { className, children, ...other } = props;
+  const { t } = useTranslation();
+
+  const menu = useMemo(() => (
+    <Menu
+      items={[
+        {
+          icon: <UserAddOutlined />,
+          label: "加签",
+          key: '0',
+          onClick: e => {
+            //e.domEvent.stopPropagation();
+            //handleAddDiagram();
+          }
+        },
+
+        {
+          icon: <ClockCircleOutlined />,
+          label: "催办",
+          key: '1',
+          onClick: e => {
+            //e.domEvent.stopPropagation();
+            //handleAddDiagram();
+          }
+        },
+        {
+          icon: <HistoryOutlined />,
+          label: "审批详情",
+          key: '2',
+          onClick: e => {
+            //e.domEvent.stopPropagation();
+            //handleAddDiagram();
+          }
+        },
+        {
+          icon: <NodeIndexOutlined />,
+          label: "审批流程图",
+          key: '3',
+          onClick: e => {
+            //e.domEvent.stopPropagation();
+            //handleAddDiagram();
+          }
+        },
+      ]}
+    />
+  ), [t]);
 
   return (
     <div className={cls("appx-arroval-bar", className)} {...other}>
@@ -27,7 +73,12 @@ export const ApprovalBar = observer((props: IApprovalBarProps) => {
         <Space>
           <Button icon={<CloseOutlined />}>驳回</Button>
           <Button type="primary" icon={<CheckOutlined />}>通过</Button>
-          <Button icon={<MoreOutlined />}></Button>
+          <Dropdown
+            overlay={menu}
+            trigger={['click']}
+          >
+            <Button icon={<MoreOutlined />}></Button>
+          </Dropdown>
         </Space>
       </Space>
     </div>
